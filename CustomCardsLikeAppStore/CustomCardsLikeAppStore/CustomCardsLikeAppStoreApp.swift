e5090c00ec75205d2456e2939652120d9f95a0d7
israman <israelmanzo814@gmail.com>
2022-05-15 21:07:16 -0400

    added card project

---
+++ CustomCardsLikeAppStore/CustomCardsLikeAppStore/CustomCardsLikeAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCardsLikeAppStoreApp.swift
+//  CustomCardsLikeAppStore
+//
+//  Created by Israel Manzo on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomCardsLikeAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
