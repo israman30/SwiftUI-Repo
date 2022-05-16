462d743a8a4df0385864de5c82f3d941438a4d8f
israman <israelmanzo814@gmail.com>
2022-05-15 22:04:24 -0400

    added new card custmized

---
+++ CustomCard-with-List/CustomCard-with-List/CustomCard_with_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCard_with_ListApp.swift
+//  CustomCard-with-List
+//
+//  Created by Israel Manzo on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomCard_with_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
