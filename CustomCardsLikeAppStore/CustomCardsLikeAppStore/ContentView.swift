ffc569ae065f159738365dcf8e6df71881fca8be
israman <israelmanzo814@gmail.com>
2022-05-15 21:51:01 -0400

    added custom card using cars framework

--- CustomCardsLikeAppStore/CustomCardsLikeAppStore/ContentView.swift
+++ CustomCardsLikeAppStore/CustomCardsLikeAppStore/ContentView.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+import Cards
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            CustomCardView()
+                .frame(width: 200, height: 200)
+        }
     }
 }
 
@@ -18,4 +21,27 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct CustomCardView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> CardHighlight {
+        let card = CardHighlight(frame: .zero)
+        card.icon = UIImage(named: "icon")
+        card.backgroundImage = UIImage(named: "background")
+        card.title = "Super Mario World"
+        card.itemTitle = "Mario"
+        card.itemSubtitle = "Super mario wordl"
+        card.buttonText = "GET"
+        card.titleSize = 25
+        
+        return card
+    }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+}
+
+class NewView: UIViewController {
+    
 }
