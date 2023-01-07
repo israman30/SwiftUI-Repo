757dd00a19aacdd956d6fcb31b3340ef1c8429af
Israel Manzo <israelmanzo@Israels-MacBook-Pro-2.local>
2023-01-06 21:59:44 -0500

    added networking class

--- Networking/Networking/ContentView.swift
+++ Networking/Networking/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+//https://jsonplaceholder.typicode.com/users
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -22,5 +24,21 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+class NetworkServices {
+    
+    static let shared = NetworkServices()
+    
+    func fetchUser() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if error != nil {
+                return
+            }
+            
+        }
+        task.resume()
     }
 }
