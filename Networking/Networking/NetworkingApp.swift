cf86095fb3139280d7baeb4bec671f01daff18cd
Israel Manzo <israelmanzo@Israels-MacBook-Pro-2.local>
2023-01-06 21:44:19 -0500

    added networking project

---
+++ Networking/Networking/NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingApp.swift
+//  Networking
+//
+//  Created by Israel Manzo on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
