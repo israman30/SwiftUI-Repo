86466ca36df58346b409001d4a7d0b2963b1f8de
Israel Manzo <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-01-11 21:29:00 -0500

    added nw project async

---
+++ Networking with async/Networking with async/Networking_with_asyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_with_asyncApp.swift
+//  Networking with async
+//
+//  Created by Israel Manzo on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Networking_with_asyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Networking with async/Networking with asyncUITests/Networking_with_asyncUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Networking_with_asyncUITestsLaunchTests.swift
+//  Networking with asyncUITests
+//
+//  Created by Israel Manzo on 1/11/23.
+//
+
+import XCTest
+
+final class Networking_with_asyncUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
