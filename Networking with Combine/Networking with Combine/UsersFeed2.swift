0cd417a0d82f7ce732816685e99ae3ef9eff090b
Israel Manzo <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-01-20 11:01:42 -0500

    added user feed 2

---
+++ Networking with Combine/Networking with Combine/UsersFeed2.swift
@@ -0,0 +1,36 @@
+//
+//  UsersFeed2.swift
+//  Networking with Combine
+//
+//  Created by Israel Manzo on 1/20/23.
+//
+
+import SwiftUI
+
+struct UsersFeed2: View {
+    
+    @StateObject private var vm = NetworkViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(vm.users) { user in
+                    Text("\(user.name)")
+                        .padding(5)
+                        .font(.title3)
+                }
+                .navigationTitle("Network Combine")
+            }
+            .onAppear {
+                vm.getUsers()
+            }
+        }
+
+    }
+}
+
+struct UsersFeed2_Previews: PreviewProvider {
+    static var previews: some View {
+        UsersFeed2()
+    }
+}
