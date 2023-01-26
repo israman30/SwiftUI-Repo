5b2be38ac67a01f137b9620408aa0e22bf9c8321
Israel Manzo <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-01-25 21:22:27 -0500

    added project ONE for modules

---
+++ ModuleONE/ModuleONE/ModuleONEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModuleONEApp.swift
+//  ModuleONE
+//
+//  Created by Israel Manzo on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ModuleONEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
