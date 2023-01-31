c947dccb9766ad84c1c1b7ebe0bfa450789a3aae
Israel Manzo <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-01-31 10:51:03 -0500

    added new thread method

--- URLSession/URLIntro-ONE/URLIntro-ONE/ContentView.swift
+++ URLSession/URLIntro-ONE/URLIntro-ONE/ContentView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            DatePicker(selection: .constant(Date()), label: {
+                Text("Date")
+            }).labelsHidden()
+            Button { calculatePrime() } label: {
+                Text("Calculate the time")
+            }
+
+            Spacer()
         }
         .padding()
+    }
+    
+    func calculatePrime() {
+        for number in 0...1_000_000 {
+            let isPrimeNumber = isPrime(number: number)
+            print("\(number) is prime: \(isPrimeNumber)")
+        }
+    }
+    
+    func isPrime(number: Int) -> Bool {
+        if number <= 1 {
+            return false
+        }
+        var i = 2
+        while i * i <= number {
+            if number % i == 0 {
+                return false
+            }
+            i = i + 2
+        }
+        return true
     }
 }
 
