a7ad453b8d7cb0eddce5e4cc0afb07a4e42c24b9
@israman30 <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-06-19 23:44:30 -0400

    fetched core data and rendering on list

--- Store JSON in Core Data/Fetch API and store in Core Data/Fetch API and store in Core Data/View/PostListView.swift
+++ Store JSON in Core Data/Fetch API and store in Core Data/Fetch API and store in Core Data/View/PostListView.swift
@@ -12,25 +12,45 @@ struct PostListView: View {
     @StateObject var vm = PostViewModel()
     @Environment(\.managedObjectContext) var context
     
+    // MARK: - Fetching data from Core Data
+    @FetchRequest(entity: Post.entity(), sortDescriptors: []) var results: FetchedResults<Post>
+    
     var body: some View {
         
         NavigationView {
             VStack {
-                if vm.posts.isEmpty {
-                    ProgressView()
-                        .onAppear {
-                            vm.fetchRequest(context: context)
+                // IF CORE DATA HAS OBJECT
+                if results.isEmpty {
+                    if vm.posts.isEmpty {
+                        ProgressView()
+                            .onAppear {
+                                vm.fetchRequest(context: context)
+                            }
+                    } else {
+                        List(vm.posts, id: \.id) { post in
+                            Text(post.title)
                         }
+                    }
                 } else {
-                    List(vm.posts, id: \.id) { post in
-                        Text(post.title)
+                    List(results) { post in
+                        Text(post.title ?? "No Title")
                     }
                 }
+                
             }
-            .navigationTitle("Fetched JSON")
+            .navigationTitle(!results.isEmpty ? "Fetched Core Data" : "Fetched JSON")
             .toolbar {
                 ToolbarItem {
                     Button {
+                        /// Clear  Core Data
+                        do {
+                            results.forEach { post in
+                                context.delete(post)
+                            }
+                            try context.save()
+                        } catch {
+                            print(error.localizedDescription)
+                        }
                         /// Clrea array Posts it will fetch again
                         self.vm.posts.removeAll()
                     } label: {
