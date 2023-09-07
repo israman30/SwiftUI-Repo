e37c60ddae1176fd98f7f6ec64254bae20b39e7d
@israman30 <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-09-06 22:40:34 -0400

    added new project for mastering networking

---
+++ Mastering Networking/Mastering Networking/App/Mastering_NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mastering_NetworkingApp.swift
+//  Mastering Networking
+//
+//  Created by Israel Manzo on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Mastering_NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
