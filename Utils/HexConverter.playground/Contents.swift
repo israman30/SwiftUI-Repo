a281cae3c0fd30baf8bd27c2720bf1e019a9d7fc
@israman30 <you@example.com>
2023-12-14 20:04:25 -0500

    a - refactored hex color extension

--- Utils/HexConverter.playground/Contents.swift
+++ Utils/HexConverter.playground/Contents.swift
@@ -1,16 +1,18 @@
 import UIKit
 
 extension UIColor {
+    
     convenience init?(hex: String) {
+        
         var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
         hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
         
         var rgb: UInt64 = 0
         
-        var r: CGFloat = 0.0
-        var g: CGFloat = 0.0
-        var b: CGFloat = 0.0
-        var a: CGFloat = 1.0
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 1.0
         
         let length = hexSanitized.count
         
@@ -18,20 +20,21 @@ extension UIColor {
         
         switch length {
         case 6:
-            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
-            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
-            b = CGFloat(rgb & 0x0000FF) / 255.0
+            red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+            green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+            blue = CGFloat(rgb & 0x0000FF) / 255.0
         case 8:
-            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
-            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
-            a = CGFloat(rgb & 0x000000FF) / 255.0
+            red = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+            green = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+            blue = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+            alpha = CGFloat(rgb & 0x000000FF) / 255.0
         default:
             return nil
         }
         
         self.init(red: r, green: g, blue: b, alpha: a)
     }
+    
     // Creating custom color method
     static func  mainColor() -> UIColor? {
         return UIColor(hex: "fcfbf0")
