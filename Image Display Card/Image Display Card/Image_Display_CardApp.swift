d8ded72c0d3a3f14c4923b0eb3049ce7f7239447
@israman30 <you@example.com>
2023-12-18 19:46:30 -0500

    e - adde new project for custom image card

---
+++ Image Display Card/Image Display Card/Image_Display_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Display_CardApp.swift
+//  Image Display Card
+//
+//  Created by Israel Manzo on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Image_Display_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- RGB generator/RGB generator/ContentView.swift
+++ RGB generator/RGB generator/ContentView.swift
@@ -6,29 +6,55 @@
 //
 
 import SwiftUI
+import Combine
+
+final class SliderComponent: ObservableObject {
+    @Published var redColor = 0.0
+    @Published var greenColor = 0.0
+    @Published var blueColor = 0.0
+}
 
 struct ContentView: View {
     
     @State var redcolor = 0.0
     @State var greenColor = 0.0
     @State var blueColor = 0.0
+    @State var sliderColor: Color = .red
+    
+    @StateObject var sliderComponent = SliderComponent()
+    
+    var rgbColor: Color {
+        Color(red: redcolor, green: greenColor, blue: blueColor)
+    }
     
     var body: some View {
         Form {
             
             Circle()
-                .foregroundStyle(Color(red: redcolor, green: greenColor, blue: blueColor))
+                .onSubmit {
+                    
+                }
+                .foregroundStyle(rgbColor)
             Section("Color") {
                 Image(systemName: "globe")
                     .imageScale(.large)
                     .foregroundStyle(.tint)
                 
                 Text("Hello, world!")
-                Slider(value: $redcolor, in: 0...100, step: 0.5)
+                Slider(value: $redcolor, in: 0...255, step: 0.5)
+                    .onChange(of: self.redcolor) {
+                        sliderColor = rgbColor
+                    }
                 Text("\(redcolor)")
-                Slider(value: $greenColor, in: 0...100, step: 0.5)
+                Slider(value: $greenColor, in: 0...255, step: 0.5)
+                    .onChange(of: self.greenColor) {
+                        sliderColor = rgbColor
+                    }
                 Text("\(greenColor)")
-                Slider(value: $blueColor, in: 0...100, step: 0.5)
+                Slider(value: $blueColor, in: 0...255, step: 0.5)
+                    .onChange(of: self.blueColor) {
+                        sliderColor = rgbColor
+                    }
                 Text("\(blueColor)")
             }
             
