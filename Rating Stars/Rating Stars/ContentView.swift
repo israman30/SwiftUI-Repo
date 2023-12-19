5f41c4dc53e15235aa736857b2f2bb060852ba67
@israman30 <you@example.com>
2023-12-18 22:17:03 -0500

    F - created custom image card

--- Rating Stars/Rating Stars/ContentView.swift
+++ Rating Stars/Rating Stars/ContentView.swift
@@ -9,16 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        StarsView(rating: 1.5, maxRating: 5)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct StarsView: View {
+    var rating: CGFloat
+    var maxRating: Int
+
+    var body: some View {
+        stars
+            .overlay(
+            GeometryReader { g in
+                let width = rating / CGFloat(maxRating) * g.size.width
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: width)
+                        .foregroundColor(.yellow)
+                }
+            }
+            .mask(stars)
+        )
+        .foregroundColor(.gray)
+    }
+    
+    var stars: some View {
+        HStack(spacing: 0) {
+           ForEach(0..<maxRating, id: \.self) { _ in
+               Image(systemName: "star.fill")
+                   .resizable()
+                   .aspectRatio(contentMode: .fit)
+           }
+       }
+    }
 }
