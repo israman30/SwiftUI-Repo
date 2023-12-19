12e67ca328a74c2a5796e6804391dff658f5eb21
@israman30 <you@example.com>
2023-12-18 21:45:16 -0500

    F - Added new project for rating starts

---
+++ Rating Stars/Rating Stars/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rating Stars
+//
+//  Created by Israel Manzo on 12/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rating Stars/Rating Stars/Rating_StarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rating_StarsApp.swift
+//  Rating Stars
+//
+//  Created by Israel Manzo on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Rating_StarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
