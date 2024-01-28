91e7a46fa776c207f1c94580603df138b1b29086
@israman30 <you@example.com>
2024-01-27 19:46:00 -0500

    F - added boolean for make toast visible

--- Custom Toast Message/Custom Toast Message/ContentView.swift
+++ Custom Toast Message/Custom Toast Message/ContentView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isVisible = false
+    
     var body: some View {
-        CustomToastView()
+        VStack(spacing: 50) {
+            
+            if isVisible {
+                CustomToastView(text: "Toast Message")
+            }
+            
+            Button {
+                isVisible = true
+            } label: {
+                Text("Tap me")
+                    .font(.title)
+            }
+            .buttonStyle(.borderedProminent)
+        }
     }
 }
 
@@ -19,10 +35,13 @@ struct ContentView: View {
 
 
 struct CustomToastView: View {
+    
+    var text: String
+    
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .font(.title)
+            Text(text)
+                .font(.title3)
         }
         .padding()
         .background(Color(.systemGray5))
