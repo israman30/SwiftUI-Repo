6a81a38c14866856e66fda1fc8129addde49b94e
@israman30 <you@example.com>
2024-02-05 21:21:51 -0500

    Fgit add . - created view model for search text

--- MapKit Autocomplete/MapKit Autocomplete/ViewModel/ViewModel.swift
+++ MapKit Autocomplete/MapKit Autocomplete/ViewModel/ViewModel.swift
@@ -5,7 +5,35 @@
 //  Created by Israel Manzo on 2/5/24.
 //
 
-import Foundation
+import SwiftUI
 import  MapKit
 
-
+class ViewModel: NSObject, ObservableObject, MKLocalSearchCompleterDelegate {
+    @Published private(set) var results: [AddressResult] = []
+    @Published var searchText = ""
+    
+    private var localSearchCompleter: MKLocalSearchCompleter {
+        let completer = MKLocalSearchCompleter()
+        completer.delegate = self
+        return completer
+    }
+    
+    func searhAddress(searchText: String) {
+        guard searchText.isEmpty == false else { return }
+        localSearchCompleter.queryFragment = searchText
+    }
+    
+    @MainActor
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        Task {
+            results = completer.results
+                .map {
+                    AddressResult(title: $0.title, subtitle: $0.subtitle)
+                }
+        }
+    }
+    
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        // ---
+    }
+}
