551ff00c0583231501ec2458334b73256b1829fd
@israman30 <you@example.com>
2024-03-28 19:00:41 -0400

    added new project for auth

---
+++ Authentication/Authentication/AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationApp.swift
+//  Authentication
+//
+//  Created by Israel Manzo on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
