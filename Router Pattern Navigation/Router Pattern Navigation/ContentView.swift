3edac55d1c88cfba4790e29eca9e19cab317ddbf
@israman30 <you@example.com>
2024-04-03 19:04:10 -0400

    Fgit add . - adding router component

--- Router Pattern Navigation/Router Pattern Navigation/ContentView.swift
+++ Router Pattern Navigation/Router Pattern Navigation/ContentView.swift
@@ -9,16 +9,73 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Home()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+class Router: ObservableObject {
+    
+    enum Route: Hashable {
+        case viewA
+        case viewB
+        case viewC
+    }
+    
+    @Published var path: NavigationPath = NavigationPath()
+    
+    @ViewBuilder
+    func view(for router: Route) -> some View {
+        switch router {
+        case .viewA:
+            EmptyView()
+        case .viewB:
+            EmptyView()
+        case .viewC:
+            EmptyView()
+        }
+    }
+    
+    func navigate(to route: Route) {
+        path.append(route)
+    }
+    
+    func navigateBack() {
+        path.removeLast()
+    }
+    
+    func goToRoot() {
+        path.removeLast(path.count)
+    }
+}
+
+struct RouterView<Content: View>: View {
+    
+    @StateObject var router = Router()
+    
+    private let content: Content
+    
+    init(@ViewBuilder content: @escaping() -> Content) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        NavigationStack(path: $router.path) {
+            content
+                .navigationDestination(for: Router.Route.self) { route in
+                    router.view(for: route)
+                }
+        }
+    }
+}
+
+struct Home: View {
+    var body: some View {
+        Text("Home View")
+            .font(.largeTitle)
+            .fontWeight(.heavy)
+    }
 }
