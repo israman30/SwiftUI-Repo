d43a17f41d6acbd9350e4b742f925374249b7933
@israman30 <you@example.com>
2024-04-03 19:46:44 -0400

    added new project for coordinator pattern

---
+++ Coordinator Pattern Navigation/Coordinator Pattern Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coordinator Pattern Navigation
+//
+//  Created by Israel Manzo on 4/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Coordinator Pattern Navigation/Coordinator Pattern Navigation/Coordinator_Pattern_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator_Pattern_NavigationApp.swift
+//  Coordinator Pattern Navigation
+//
+//  Created by Israel Manzo on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Coordinator_Pattern_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
