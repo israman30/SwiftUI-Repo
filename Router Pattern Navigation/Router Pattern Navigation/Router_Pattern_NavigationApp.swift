c5371e275ac6e09772a55afea4f788f761373196
@israman30 <you@example.com>
2024-04-03 18:55:23 -0400

    added router pattern navigation project

---
+++ Router Pattern Navigation/Router Pattern Navigation/Router_Pattern_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Router_Pattern_NavigationApp.swift
+//  Router Pattern Navigation
+//
+//  Created by Israel Manzo on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Router_Pattern_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
