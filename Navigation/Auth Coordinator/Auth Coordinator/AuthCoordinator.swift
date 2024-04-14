9ce0ea87c060462b114801a2e0bf13355e439600
@israman30 <you@example.com>
2024-04-13 20:14:57 -0400

    Fgit add . git status - added auth coordinator

---
+++ Navigation/Auth Coordinator/Auth Coordinator/AuthCoordinator.swift
@@ -0,0 +1,78 @@
+//
+//  AuthCoordinator.swift
+//  Auth Coordinator
+//
+//  Created by Israel Manzo on 4/13/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum AuthPage {
+    case login
+    case forgotPassword
+}
+
+final class AuthCoordinator: Hashable {
+    
+    @Binding var navigationPath: NavigationPath
+    
+    private var id: UUID
+    private var output: Output?
+    private var page: AuthPage
+    
+    struct Output {
+        var goToMain: () -> ()
+    }
+    
+    init(page: AuthPage, navigationPath: Binding<NavigationPath>, output: Output? = nil) {
+        id = UUID()
+        self.page = page
+        self.output = output
+        self._navigationPath = navigationPath
+    }
+    
+    @ViewBuilder
+    func view() -> some View {
+        switch self.page {
+        case .login:
+            loginView()
+        case .forgotPassword:
+            forgotPassword()
+        }
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: AuthCoordinator,rhs: AuthCoordinator) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    private func loginView() -> some View {
+        let loginView = LoginView(output: .init(goToMain: {
+            self.output?.goToMain()
+        }, goToForgotPassword: {
+            self.gotToForgotPasswordSite()
+        }))
+        return loginView
+    }
+    
+    private func forgotPassword() -> some View {
+        let forgotPassword = ForgotPasswordView(output: .init(goToForgotPasswordSite: {
+            self.gotToForgotPasswordSite()
+        }))
+        return forgotPassword
+    }
+    
+    private func gotToForgotPasswordSite() {
+        if let url = URL(string: "somesite.com") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    func push<T>(_ value: T) where T: Hashable {
+        navigationPath.append(value)
+    }
+}
