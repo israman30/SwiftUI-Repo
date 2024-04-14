172d660bbc0bc3820faa00dc39d2083c3dc09aca
@israman30 <you@example.com>
2024-04-13 21:10:06 -0400

    F - added custom alignment and extension view with alignment function

--- Alignments/Alignments/ContentView.swift
+++ Alignments/Alignments/ContentView.swift
@@ -23,3 +23,30 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+enum CustomAlignment {
+    case top
+    case leading
+    case trailing
+    case bottom
+    case center
+}
+
+extension View {
+    
+    @ViewBuilder
+    func alignment(at alignment: CustomAlignment) -> some View {
+        switch alignment {
+        case .top:
+            self.frame(maxHeight: .infinity, alignment: .top)
+        case .bottom:
+            self.frame(maxHeight: .infinity, alignment: .bottom)
+        case .leading:
+            self.frame(maxWidth: .infinity, alignment: .leading)
+        case .trailing:
+            self.frame(maxWidth: .infinity, alignment: .trailing)
+        case .center:
+            self.frame(maxWidth: .infinity, alignment: .center)
+        }
+    }
+}
