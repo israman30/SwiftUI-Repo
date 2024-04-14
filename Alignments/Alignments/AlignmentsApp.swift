6976b7831d47f923816dbf475301dc9e2eb1049a
@israman30 <you@example.com>
2024-04-13 21:05:05 -0400

    added project for alignment

---
+++ Alignments/Alignments/AlignmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignmentsApp.swift
+//  Alignments
+//
+//  Created by Israel Manzo on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AlignmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alignments/Alignments/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Alignments
+//
+//  Created by Israel Manzo on 4/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .background(Color.yellow)
+    }
+}
+
+#Preview {
+    ContentView()
+}
