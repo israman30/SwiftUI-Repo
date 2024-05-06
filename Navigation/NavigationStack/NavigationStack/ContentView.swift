d4cfa170a5a6d9e1b304aa71ea49c822c0aeeb0a
Israel Manzo <israelmanzo814@gmail.com>
2024-05-05 21:27:13 -0400

    added navigation stack view with object and list users

--- Navigation/NavigationStack/NavigationStack/ContentView.swift
+++ Navigation/NavigationStack/NavigationStack/ContentView.swift
@@ -7,18 +7,41 @@
 
 import SwiftUI
 
-struct Users {
+struct Users: Identifiable {
     let name: String
+    var id: String {
+        name
+    }
+    
+    
 }
 
 class ViewModel: ObservableObject {
     
+    private var users: Users
+    
+    init(users: Users) {
+        self.users = users
+    }
 }
 
 struct ContentView: View {
+
+    @State var sample: [Users] = [
+        Users(name: "John Doe"),
+        Users(name: "Fred Flinstone"),
+        Users(name: "Peter Parker"),
+        Users(name: "Tom Sawyer")
+    ]
+    
     var body: some View {
         NavigationStack {
-            
+            List {
+                ForEach(sample, id: \.id) { users in
+                    Text(users.name)
+                }
+            }
+            .navigationTitle("Navigation Stack")
         }
     }
 }
