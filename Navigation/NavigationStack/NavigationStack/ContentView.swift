72b88f15fd512f494035c9b32042f8864d0c7c04
Israel Manzo <israelmanzo814@gmail.com>
2024-05-05 21:14:03 -0400

    added new project for navigation stack

---
+++ Navigation/NavigationStack/NavigationStack/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  NavigationStack
+//
+//  Created by Israel Manzo on 5/5/24.
+//
+
+import SwiftUI
+
+struct Users {
+    let name: String
+}
+
+class ViewModel: ObservableObject {
+    
+}
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
