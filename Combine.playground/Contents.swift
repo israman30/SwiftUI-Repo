abc7daaa11d2f751ff2989d17c25207598f5773f
Israel Manzo <israelmanzo814@gmail.com>
2024-05-29 21:41:05 -0400

    F -add publisher and sibscriber sample

--- Combine.playground/Contents.swift
+++ Combine.playground/Contents.swift
@@ -6,7 +6,25 @@ import Combine
  Combine includes several built-in publishers like Just, `NotificationCenter.Publisher`, and many others.
  */
 
+// Just: Emits a single value and then completes
 let aPublisher = Just("Single value")
 aPublisher.sink { value in
     print("Received: \(value)")
 }
+
+// NotificationCenter Publisher: Emits notifications from NotificationCenter
+let notificationPublisher = NotificationCenter.default.publisher(for: .NSCalendarDayChanged)
+notificationPublisher.sink { notification in
+    print("Day changed: \(notification)")
+}
+
+
+// MARK: 2. Subscribers
+/**
+ Subscribers receive values from publishers. Combine offers built-in subscribers such as `sink` and `assign`.
+ */
+// Sink: Handles each received value and completion event.
+let aSubscriber = Just("Sink value")
+    .sink { value in
+        print("Received value: \(value)")
+    }
