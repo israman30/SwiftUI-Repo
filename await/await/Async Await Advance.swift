4403a6cf9428ac8c5b3834bce701063bde7be3b6
Israel Manzo <israelmanzo814@gmail.com>
2024-07-27 21:11:30 -0400

    added async/await

--- await/await/Async Await Advance.swift
+++ await/await/Async Await Advance.swift
@@ -31,3 +31,43 @@ struct SomeOtherClass: View {
             }
     }
 }
+
+
+// MARK: - Concurrent Data Fetching
+
+func fethcMultipleData() async {
+    async let dataA = Network().fetchData(url: "example.com/A")
+    async let dataB = Network().fetchData(url: "example.com/B")
+    async let dataC = Network().fetchData(url: "example.com/C")
+    async let dataD = Network().fetchData(url: "example.com/D")
+    
+    do {
+        let results = try await (dataA, dataB, dataC, dataD)
+        print("Results: ", results)
+    } catch {
+        print("Error fetching data", error.localizedDescription)
+    }
+}
+
+// MARK: - Handling Timeouts and Cancellation
+
+func fetchWithTimeOut(url: String) async throws -> Data {
+    let task = Task {
+        try await Network().fetchData(url: url)
+    }
+    let timeoutTask = Task {
+        try await Task.sleep(nanoseconds: 5_000_000)
+        task.cancel()
+    }
+    return try await task.value
+}
+
+func fetching() {
+    Task {
+        do {
+            let data = try await Network().fetchData(url: "example.com")
+        } catch {
+            print("Operation timeout was cancelled: ", error.localizedDescription)
+        }
+    }
+}
