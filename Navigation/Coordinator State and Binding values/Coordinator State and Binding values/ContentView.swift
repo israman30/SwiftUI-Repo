de489802af31fc976a22b8a6af920bd0eb153154
Israel Manzo <israelmanzo814@gmail.com>
2024-10-20 19:49:52 -0400

    creating new coordinator

--- Navigation/Coordinator State and Binding values/Coordinator State and Binding values/ContentView.swift
+++ Navigation/Coordinator State and Binding values/Coordinator State and Binding values/ContentView.swift
@@ -7,6 +7,33 @@
 
 import SwiftUI
 
+enum Page: Hashable {
+    case home
+    case detail
+}
+
+class Coordinator: ObservableObject {
+    @Published var path = NavigationPath()
+    
+    func push(_ page: Page) {
+        path.append(page)
+    }
+    
+    func dismiss() {
+        path.removeLast()
+    }
+    
+    @ViewBuilder
+    func buld(_ page: Page) -> some View {
+        switch page {
+            case .home:
+            Text("Home")
+        case .detail:
+            Text("Detail")
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
