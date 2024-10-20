c2b0f0bff34c502872c94ccfc82e940bc622622d
Israel Manzo <israelmanzo814@gmail.com>
2024-10-19 22:12:40 -0400

    added new progress view project

---
+++ Custom ProgressView/Custom ProgressView/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Custom ProgressView
+//
+//  Created by Israel Manzo on 10/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            DefaultProgressView()
+        }
+        .padding()
+    }
+}
+
+struct DefaultProgressView: View {
+    
+    @State var progress: Double = 0.5
+    
+    var body: some View {
+        ProgressView(value: progress)
+            .progressViewStyle(.automatic)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Custom ProgressView/Custom ProgressView/Custom_ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_ProgressViewApp.swift
+//  Custom ProgressView
+//
+//  Created by Israel Manzo on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
