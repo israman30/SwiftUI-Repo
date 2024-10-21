880da1e5cd29e6194577e23a8e7eec0a27d9b611
Israel Manzo <israelmanzo814@gmail.com>
2024-10-21 19:50:34 -0400

    added user state for sheet

--- Navigation/Coordinator State and Binding values/Coordinator State and Binding values/SheetView.swift
+++ Navigation/Coordinator State and Binding values/Coordinator State and Binding values/SheetView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SheetView: View {
     
     @Environment(\.dismiss) var dismiss
+    @State var userActivity: Bool = false
     
     var body: some View {
         VStack {
@@ -25,6 +26,13 @@ struct SheetView: View {
                 
             }
             Spacer()
+            HStack {
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(userActivity ? .green : .red)
+                Text("User activity is \(userActivity ? "active" : "not active")")
+                    .font(.title)
+            }
             Text("this is a sheet")
             Spacer()
         }
