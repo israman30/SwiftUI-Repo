3c93c9a965c5eba0d6fefcab381de84d4958a547
Israel Manzo <israelmanzo814@gmail.com>
2024-10-20 21:00:58 -0400

    added a view model

---
+++ Navigation/Coordinator State and Binding values/Coordinator State and Binding values/MyViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MyViewModel.swift
+//  Coordinator State and Binding values
+//
+//  Created by Israel Manzo on 10/20/24.
+//
+
+import Foundation
+
+final class MyViewModel: ObservableObject {
+    @Published var text: String = "This is data from the View Model"
+    @Published var isLoading: Bool = false
+}
