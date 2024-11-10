1bd4107f92ead13c06a3cace51ce243c1870d5f0
Israel Manzo <israelmanzo814@gmail.com>
2024-11-09 21:28:44 -0500

    added new project for tag collection

---
+++ Label Tag Collection/Label Tag Collection/Label_Tag_CollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Label_Tag_CollectionApp.swift
+//  Label Tag Collection
+//
+//  Created by Israel Manzo on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Label_Tag_CollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
