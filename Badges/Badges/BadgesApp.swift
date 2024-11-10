b4562ddfd58b71f6589a9380ded55530aafb983d
Israel Manzo <israelmanzo814@gmail.com>
2024-11-09 20:16:41 -0500

    Added new xcode porject for badges

---
+++ Badges/Badges/BadgesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadgesApp.swift
+//  Badges
+//
+//  Created by Israel Manzo on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BadgesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
