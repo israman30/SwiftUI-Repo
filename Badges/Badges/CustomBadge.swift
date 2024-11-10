f3e1faecbc8bb5160add2b7c16c66abbe1863a7f
Israel Manzo <israelmanzo814@gmail.com>
2024-11-09 20:30:36 -0500

    added a custom badge view

---
+++ Badges/Badges/CustomBadge.swift
@@ -0,0 +1,64 @@
+//
+//  CustomBadge.swift
+//  Badges
+//
+//  Created by Israel Manzo on 11/9/24.
+//
+
+import SwiftUI
+
+struct CustomBadge: View {
+    var body: some View {
+        List {
+            VStack {
+                Image(systemName: "house.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 50)
+                Text("Home")
+                    .font(.system(size: 15))
+            }
+            .foregroundStyle(.blue)
+            .customBadge(count: 7)
+        }
+    }
+}
+
+#Preview {
+    CustomBadge()
+}
+
+struct Badge: ViewModifier {
+    let count: Int
+    let maxCount: Int
+    
+    func body(content: Content) -> some View {
+        if count > 0 {
+            ZStack(alignment: .topTrailing) {
+                content
+                HStack(spacing: 0) {
+                    Text(min(count, maxCount), format: .number)
+                    
+                    if count > maxCount {
+                        Text("+")
+                    }
+                }
+                .font(.system(size: 14))
+                .foregroundStyle(.white)
+                .padding(.horizontal, 5)
+                .padding(.vertical, count > 9 ? 7 : 5) //// when the badge is a single digit, I like the badge to be close to a circle shape
+                .background(.red)
+                .frame(height: count > 9 ? 17 : 19)
+                .clipShape(Capsule())
+            }
+        } else {
+            content // else no badge display
+        }
+    }
+}
+
+extension View {
+    func customBadge(count: Int = 0, maxCount: Int = 99) -> some View {
+        modifier(Badge(count: count, maxCount: maxCount))
+    }
+}
