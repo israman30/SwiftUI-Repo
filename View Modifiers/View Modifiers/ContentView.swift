689bb5ce16f98debe0d3cfdf9c42f813d23f11a4
Israel Manzo <israelmanzo814@gmail.com>
2024-11-12 20:02:23 -0500

    added new project for view modifiers

---
+++ View Modifiers/View Modifiers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  View Modifiers
+//
+//  Created by Israel Manzo on 11/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ View Modifiers/View Modifiers/View_ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  View_ModifiersApp.swift
+//  View Modifiers
+//
+//  Created by Israel Manzo on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct View_ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
