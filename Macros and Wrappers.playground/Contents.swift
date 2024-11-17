cd91af3960f7c910e7cd84b9a369b98c0a867c50
Israel Manzo <israelmanzo814@gmail.com>
2024-11-16 19:45:41 -0500

    added entry macros class

--- Macros and Wrappers.playground/Contents.swift
+++ Macros and Wrappers.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+import SwiftUI
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
+// MARK: - @Entry -
+/**
+ The `@Entry` macro in SwiftUI allows you to define custom environment values without writing boilerplate code. While introduced in Xcode 16, you can use it from `iOS 13 and up` since it’s a Swift Macro that generates backward-compatible code.
+ The new macro can be used for environment values, as well as for transaction, container, and focused values. In this article, we’ll be focusing on environment values only.
+ */
+
+class ColorTheme {
+    var primaryColor: Color
+    
+    init(primaryColor: Color) {
+        self.primaryColor = primaryColor
+    }
+}
+
+extension EnvironmentValues {
+    @Entry var colorTheme: ColorTheme = .init(primaryColor: .accentColor)
+}
+
+struct ThemeView: View {
+    @Environment(\.colorTheme) private var colorTheme
+    var body: some View {
+        Text("Hello, World!")
+            .foregroundStyle(colorTheme.primaryColor)
+    }
+}
