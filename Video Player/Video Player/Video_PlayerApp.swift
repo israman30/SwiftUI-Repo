90b79a44285507605735ae4278309656c1a6db10
Israel Manzo <israelmanzo814@gmail.com>
2024-11-23 18:56:06 -0500

    added a new xcode project for video player

---
+++ Video Player/Video Player/Video_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_PlayerApp.swift
+//  Video Player
+//
+//  Created by Israel Manzo on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Video_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
