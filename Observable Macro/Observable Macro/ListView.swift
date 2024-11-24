8bfdbd03cd141863ed0b2e72975705e9ab790d8f
Israel Manzo <israelmanzo814@gmail.com>
2024-11-23 21:53:05 -0500

    added default mock data

--- Observable Macro/Observable Macro/ListView.swift
+++ Observable Macro/Observable Macro/ListView.swift
@@ -16,6 +16,16 @@ struct Book: Identifiable {
 
 class BookViewModel: ObservableObject {
     @Published var books: [Book] = []
+    @Published var isFavorite: Bool = false
+    
+    init() {
+        books = [
+            .init(name: "Peter Pan", isbm: "1111222"),
+            .init(name: "Harry Pother", isbm: "222333"),
+            .init(name: "Star Wars", isbm: "333444"),
+            .init(name: "TomSawyer", isbm: "444555"),
+        ]
+    }
     
     func addBook(_ book: Book) {
         books.append(book)
@@ -30,7 +40,15 @@ struct ListView: View {
         NavigationView {
             List {
                 ForEach(vm.books) { book in
-                    Text("\(book)")
+                    HStack {
+                        Text("\(book.name)")
+                            .padding(.vertical, 5)
+                        Spacer()
+                        Image(systemName: vm.isFavorite ? "star.fill" : "star")
+                            .onTapGesture {
+                                self.vm.isFavorite.toggle()
+                            }
+                    }
                 }
             }
             .navigationTitle("Books")
