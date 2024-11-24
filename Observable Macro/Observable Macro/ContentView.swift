a3f490dec882cb49b2fc39db10d4b2ea57b6dda9
Israel Manzo <israelmanzo814@gmail.com>
2024-11-23 21:40:47 -0500

    added new xcode project for obaservable macros

---
+++ Observable Macro/Observable Macro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Observable Macro
+//
+//  Created by Israel Manzo on 11/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Observable Macro/Observable Macro/Observable_MacroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Observable_MacroApp.swift
+//  Observable Macro
+//
+//  Created by Israel Manzo on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Observable_MacroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
