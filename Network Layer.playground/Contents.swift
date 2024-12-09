cb61cc5e8dc09ae0fbf42958ee0d34004b0ec9fe
Israel Manzo <israelmanzo814@gmail.com>
2024-12-09 15:13:24 -0500

    added protocol for network request

--- Network Layer.playground/Contents.swift
+++ Network Layer.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
 
+protocol Endpoint {
+    var baseURL: URL { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String: String]? { get }
+    var parameters: [String: Any]? { get }
+}
+
+extension Endpoint {
+    func urlRequest() throws -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.allHTTPHeaderFields = headers
+        
+        if let paramenters = parameters {
+            if method == .get {
+                var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+                components?.queryItems = paramenters.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
+                request.url = components?.url
+            } else {
+                request.httpBody = try JSONSerialization.data(withJSONObject: paramenters)
+            }
+        }
+        return request
+    }
+}
+
+protocol NetworkManager {
+    func fetch<T: Decodable>(from endpoint: Endpoint) async throws -> T
+}
