22d12d8e8c17dcc5aa449088e2992d61d023bf52
Israel Manzo <israelmanzo814@gmail.com>
2024-12-10 19:53:07 -0500

    added project for animate search field

---
+++ Animated Search Field/Animated Search Field/Animated_Search_FieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_Search_FieldApp.swift
+//  Animated Search Field
+//
+//  Created by Israel Manzo on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Animated_Search_FieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated Search Field/Animated Search Field/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animated Search Field
+//
+//  Created by Israel Manzo on 12/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
