02cc4ac0c292a2eb2ce6e3d8d8dce829de4bd92d
Israel Manzo <israelmanzo814@gmail.com>
2024-12-10 19:56:55 -0500

    added properties

--- Animated Search Field/Animated Search Field/ContentView.swift
+++ Animated Search Field/Animated Search Field/ContentView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var labels: [String] = ["'Food'", "'Restaurants'", "'Groceries'", "'Beverages'", "'Bread'", "'Pizza'", "'Biryani'", "'Burger'", "'Bajji'", "'Noodles'", "'Soup'", "'Sandwich'", "'Biscuits'", "'Chocolates'"]
+    @State var currentIndex: Int = 0
+    @State var currentLabel: String = "'Food'"
+    @State var nextLabel: String = "'Restaurants'"
+    @State var currentOpacity: Double = 1
+    @State var nextOpacity: Double = 0
+    @State private var currentOffset: CGFloat = 0
+    @State private var nextOffset: CGFloat = 20
+    @State private var timer: Timer? = nil
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
