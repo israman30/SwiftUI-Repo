b47b7b237845f54d65469fc86facf831134c8844
Israel Manzo <israelmanzo814@gmail.com>
2024-12-16 20:06:08 -0500

    added alert controller

--- Alert Component/Alert Component/ContentView.swift
+++ Alert Component/Alert Component/ContentView.swift
@@ -7,15 +7,30 @@
 
 import SwiftUI
 
+struct AlertObject {
+    let title: String
+    let message: String
+}
+
 struct ContentView: View {
+    @State var isPresented: Bool = false
+    @State var alertObject = AlertObject(title: "Alert title", message: "This is a message.!")
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button("Show Alert") {
+                self.isPresented.toggle()
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
+        .alert(isPresented: $isPresented) {
+            Alert(title: Text(alertObject.title), message: Text(alertObject.message), dismissButton: .default(Text("Ok")))
+        }
     }
 }
 
