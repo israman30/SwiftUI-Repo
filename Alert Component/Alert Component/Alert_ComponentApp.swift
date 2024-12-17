d44837726155d72aca760267e98872efac7382c2
Israel Manzo <israelmanzo814@gmail.com>
2024-12-16 19:59:37 -0500

    Added project for alert component

---
+++ Alert Component/Alert Component/Alert_ComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alert_ComponentApp.swift
+//  Alert Component
+//
+//  Created by Israel Manzo on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Alert_ComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alert Component/Alert Component/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alert Component
+//
+//  Created by Israel Manzo on 12/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
