b21b3bf8327d07127c92c3eccf8c9973900962e1
Israel Manzo <israelmanzo814@gmail.com>
2025-01-02 21:57:06 -0500

    added a new project for accessibility

---
+++ Accesibility Components/Accesibility Components/Accesibility_ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accesibility_ComponentsApp.swift
+//  Accesibility Components
+//
+//  Created by Israel Manzo on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Accesibility_ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
