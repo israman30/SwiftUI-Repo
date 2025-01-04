d7e9ff45aab7b4eda7290876ce49bd91d8caff66
Israel Manzo <israelmanzo814@gmail.com>
2025-01-04 12:31:11 -0500

    added new project for swift test

---
+++ Swift Test/Swift Test/Swift_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_TestApp.swift
+//  Swift Test
+//
+//  Created by Israel Manzo on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
