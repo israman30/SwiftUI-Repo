3efb9fb4d5ebf3b3581259095ac5a09d001c9b23
Israel Manzo <israelmanzo814@gmail.com>
2025-01-04 13:03:40 -0500

    added view model

---
+++ Swift Test/Swift Test/Mocking Network Connection/PostViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  PostViewModel.swift
+//  Swift Test
+//
+//  Created by Israel Manzo on 1/4/25.
+//
+
+import SwiftUI
+import Observation
+
+enum PostState {
+    case loaded(_ posts: [Post])
+    case loading
+    case noLoaded
+    case error(_ error: Error)
+}
+
+@Observable
+class PostViewModel {
+    var postState: PostState = .noLoaded
+    private let networkService: NetworkManager
+    
+    init(networkService: NetworkManager) {
+        self.networkService = networkService
+    }
+    
+    func fetchPost() async {
+        postState = .loading
+        do {
+            let posts = try await networkService.fetPosts()
+            postState = .loaded(posts)
+        } catch {
+            postState = .error(error)
+        }
+    }
+    
+    func createPost(content: String) async throws -> Post {
+        return try await networkService.createPost(contents: content)
+    }
+}
