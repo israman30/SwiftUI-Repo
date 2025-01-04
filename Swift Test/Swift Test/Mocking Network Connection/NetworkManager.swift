c6367f2c70c451c73695a25fb46378975825b1bb
Israel Manzo <israelmanzo814@gmail.com>
2025-01-04 12:40:57 -0500

    creating network layer

---
+++ Swift Test/Swift Test/Mocking Network Connection/NetworkManager.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkManager.swift
+//  Swift Test
+//
+//  Created by Israel Manzo on 1/4/25.
+//
+
+import SwiftUI
+
+struct Post: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class NetworkManager {
+    let urlSession: URLSession
+    let baseUrl: URL = URL(string: "https://jsonplaceholder.typicode.com/")!
+    
+    init(urlSession: URLSession) {
+        self.urlSession = urlSession
+    }
+    
+    func fetPosts() async throws -> [Post] {
+        let url = baseUrl.appending(path: "posts")
+        let (data, _ ) = try await urlSession.data(from: url)
+        return try JSONDecoder().decode([Post].self, from: data)
+    }
+    
+    func createPost(contents: String) async throws -> Post {
+        let url = baseUrl.appending(path: "posts")
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let body = ["contents":contents]
+        request.httpBody = try JSONEncoder().encode(body)
+        
+        let (data, _) = try await urlSession.data(for: request)
+        return try JSONDecoder().decode(Post.self, from: data)
+    }
+}
