98cef8c0a372d32f34487d50a3494aa2516405bd
Israel Manzo <israelmanzo814@gmail.com>
2025-01-12 12:03:12 -0500

    added new project for scroll view

---
+++ Navigation/ScrollView/ScrollView/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  ScrollView
+//
+//  Created by Israel Manzo on 1/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let colors: [Color] = [
+        .red, .green, .blue, .yellow, .cyan, .pink, .purple, .gray, .brown, .orange, .indigo, .mint, .primary, .secondary
+    ]
+    
+    var body: some View {
+        ZStack(alignment:. bottomTrailing) {
+            ScrollView {
+                VStack(spacing: 24) {
+                    ForEach(colors, id: \.self) { color in
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(color)
+                            .frame(width: 360, height: 240)
+                            .cornerRadius(10)
+                    }
+                }
+            }
+            
+            buttons
+        }
+    }
+    
+    private var buttons: some View {
+        VStack {
+            Button {
+                
+            } label: {
+                Image(systemName: "arrow.up.circle.fill")
+                    .resizable()
+                    .frame(width: 56, height: 56)
+                    .foregroundStyle(.white, .black)
+            }
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "arrow.down.circle.fill")
+                    .resizable()
+                    .frame(width: 56, height: 56)
+                    .foregroundStyle(.white, .black)
+            }
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "arrow.up.and.down.circle.fill")
+                    .resizable()
+                    .frame(width: 56, height: 56)
+                    .foregroundStyle(.white, .black)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation/ScrollView/ScrollView/ScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewApp.swift
+//  ScrollView
+//
+//  Created by Israel Manzo on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
