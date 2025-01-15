4dd22e26345e040f48b53b4df78b7e6553a63f47
Israel Manzo <israelmanzo814@gmail.com>
2025-01-15 14:45:34 -0500

    added new project for haptics

---
+++ Haptics Manager/Haptics Manager/Haptics_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haptics_ManagerApp.swift
+//  Haptics Manager
+//
+//  Created by Israel Manzo on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Haptics_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
