08d467108dbca04484e9bfd521a0831169e092ef
Israel Manzo <israelmanzo814@gmail.com>
2025-02-11 15:38:58 -0500

    added new project for charts

---
+++ Chart Swift/Chart Swift/Chart_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chart_SwiftApp.swift
+//  Chart Swift
+//
+//  Created by Israel Manzo on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Chart_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chart Swift/Chart Swift/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Chart Swift
+//
+//  Created by Israel Manzo on 2/11/25.
+//
+
+import SwiftUI
+import Charts
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
