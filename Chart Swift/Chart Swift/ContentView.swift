e583cddf15fac6e2be92f6bfc181c97e223a82d4
Israel Manzo <israelmanzo814@gmail.com>
2025-02-11 15:45:36 -0500

    adding chart model with a mark chart view

--- Chart Swift/Chart Swift/ContentView.swift
+++ Chart Swift/Chart Swift/ContentView.swift
@@ -8,18 +8,47 @@
 import SwiftUI
 import Charts
 
+struct SomeData: Identifiable {
+    var id = UUID()
+    let month: String
+    let sales: Double
+}
+
+let someData: [SomeData] = [
+    .init(month: "Jan", sales: 100),
+    .init(month: "Feb", sales: 200),
+    .init(month: "Mar", sales: 300),
+    .init(month: "Apr", sales: 400),
+    .init(month: "May", sales: 500),
+    .init(month: "Jun", sales: 600),
+    .init(month: "Jul", sales: 700),
+    .init(month: "Aug", sales: 800),
+    .init(month: "Sep", sales: 900),
+]
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ChartView()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ChartView: View {
+    var body: some View {
+        Chart {
+            ForEach(someData) { data in
+                BarMark(
+                    x: .value("Month", data.month),
+                    y: .value("Sales", data.sales)
+                )
+            }
+        }
+        .frame(height: 300)
+        .padding()
+    }
 }
