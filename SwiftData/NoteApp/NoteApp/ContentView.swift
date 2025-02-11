8dfcb4b04d4773e99f98cd4ef6981e79cef96a05
Israel Manzo <israelmanzo814@gmail.com>
2025-02-10 22:28:22 -0500

    added new project for swift data

---
+++ SwiftData/NoteApp/NoteApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteApp
+//
+//  Created by Israel Manzo on 2/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftData/NoteApp/NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Israel Manzo on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
