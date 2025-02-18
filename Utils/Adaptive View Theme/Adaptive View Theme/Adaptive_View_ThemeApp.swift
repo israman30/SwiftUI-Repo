47f5d02d24db720e02f65450aa9283bea4dca48b
Israel Manzo <israelmanzo814@gmail.com>
2025-02-18 10:21:34 -0500

    added adaptive view component

---
+++ Utils/Adaptive View Theme/Adaptive View Theme/Adaptive_View_ThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adaptive_View_ThemeApp.swift
+//  Adaptive View Theme
+//
+//  Created by Israel Manzo on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Adaptive_View_ThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Utils/Adaptive View Theme/Adaptive View Theme/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Adaptive View Theme
+//
+//  Created by Israel Manzo on 2/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            AdaptiveView {
+                Text("Hello, world!")
+            } dark: {
+                Text("Hello, world!")
+            }
+
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+// Used for custom components for support light and dark theme.
+struct AdaptiveView<T: View, U: View>: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    let light: T
+    let dark: U
+    
+    init(light: T, dark: U) {
+        self.light = light
+        self.dark = dark
+    }
+    
+    init(light: () -> T, dark: () -> U) {
+        self.light = light()
+        self.dark = dark()
+    }
+    
+    @ViewBuilder
+    var body: some View {
+        switch colorScheme {
+        case .light:
+            light
+        default:
+            dark
+        }
+    }
+}
