9eec5c52e55e3549e09c180d464eebcb99a51c56
Israel Manzo <israelmanzo814@gmail.com>
2025-04-02 13:54:19 -0400

    updating view model file

--- Pagination/Pagination with Fetching/Pagination with Fetching/ContentView.swift
+++ Pagination/Pagination with Fetching/Pagination with Fetching/ContentView.swift
@@ -40,46 +40,4 @@ struct ContentView: View {
     ContentView()
 }
 
-class MyViewModel: ObservableObject {
-    @Published var animes: [Anime] = []
-    
-    var page: Int = 1
-    var lastPageVisited: Int = -1
-    
-    func loadAnimes() async {
-        guard let url = URL(string: "https://api.jikan.moe/v4/top/anime?page=\(page)") else { return }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if data.isEmpty {
-                return
-            }
-            
-            let decodedData = try JSONDecoder().decode(JikanMoeResponse.self, from: data)
-            
-            /// Assign the last page of the retreived data
-            lastPageVisited = decodedData.pagination.lastVisiblePage
-            
-            animes.append(contentsOf: decodedData.data)
-            
-            ///Increment the page so that when the function is triggered again, it is ready to render the next page.
-            page += 1
-        } catch {
-            print(error)
-        }
-    }
-    
-    func shouldLoadPagination(id: Int) async {
-        if animes.last?.id == id && lastPageVisited >= page {
-            await loadAnimes()
-        }
-    }
-    
-    func refresh() async {
-        page = 1
-        lastPageVisited = -1
-        animes.removeAll()
-        
-        await loadAnimes()
-    }
-}
+
