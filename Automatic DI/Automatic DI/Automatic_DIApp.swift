bf9e18b1ef9aac776dde766ac490fe35c98b99c3
Israel Manzo <israelmanzo814@gmail.com>
2025-04-16 14:04:48 -0400

    added a new DI project

---
+++ Automatic DI/Automatic DI/Automatic_DIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Automatic_DIApp.swift
+//  Automatic DI
+//
+//  Created by Israel Manzo on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Automatic_DIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
