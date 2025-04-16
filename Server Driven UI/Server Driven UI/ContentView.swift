bc30dad49478289dbed8e0658affcb94f46e0192
Israel Manzo <israelmanzo814@gmail.com>
2025-04-16 15:58:39 -0400

    added project for serve driven UI

---
+++ Server Driven UI/Server Driven UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Server Driven UI
+//
+//  Created by Israel Manzo on 4/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
