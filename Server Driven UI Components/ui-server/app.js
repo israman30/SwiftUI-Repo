ad8038f761297ed04d8ec8f6e6b412673690c0c4
Israel Manzo <israelmanzo814@gmail.com>
2025-04-17 09:49:08 -0400

    update app.js

---
+++ Server Driven UI Components/ui-server/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World' })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up and running')
+})
