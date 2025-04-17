d706914504d93a3be928bf2d99420d6f8fb9343c
Israel Manzo <israelmanzo814@gmail.com>
2025-04-17 12:57:28 -0400

    fetching data

--- Server Driven UI Components/Express/Express/NetworkManager.swift
+++ Server Driven UI Components/Express/Express/NetworkManager.swift
@@ -3,12 +3,16 @@
 //  Express
 //
 //  Created by Israel Manzo on 4/17/25.
-//
+// http://localhost:3000/ui-list
 
 import Foundation
 
+enum ErrorNetwork: Error {
+    case errorResponse
+}
+
 struct Endpoint {
-    static let baseURL = "https://localhost:3000/"
+    static let baseURL = "http://localhost:3000/"
     static let uiList = "/ui-list"
 }
 
@@ -17,7 +21,7 @@ class NetworkManager {
         let (data, response) = try await URLSession.shared.data(from: URL(string: urlString)!)
         
         guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-            fatalError("Status code is not 200")
+            throw ErrorNetwork.errorResponse
         }
         
         return try JSONDecoder().decode(Template.self, from: data)
