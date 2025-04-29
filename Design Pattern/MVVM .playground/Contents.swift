035a4aeb4af35a2816338770de9a4d7df00eb0b8
Israel Manzo <israelmanzo814@gmail.com>
2025-04-29 09:28:25 -0400

    added a new file for mvvm

---
+++ Design Pattern/MVVM .playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+/**
+ `MVVM` `(Model-View-ViewModel)` is a software design pattern that separates an application into three interconnected parts: the `Model`,
+ `View`, and `ViewModel`.
+ This separation helps organize code, making it more maintainable, testable, and reusable.
+ The `ViewModel` acts as an intermediary between the `View` and `Model`, handling presentation logic and data binding.
+ */
+/**
+ `Components Overview and their roles
+ - `View (Controller - UIKit):` It only performs things related to UI — Show/get information. Part of the view layer
+ - `View Model`: It receives information from VC, handles all this information, and sends it back to VC.
+ - `Model: `This is only your model, nothing much here. It’s the same model as in MVC. It is used by VM and updates whenever VM sends new updates
+ */
