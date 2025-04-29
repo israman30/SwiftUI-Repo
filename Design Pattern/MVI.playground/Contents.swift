a14c235e47cad286bfc49fbc0ace995b730ecdad
Israel Manzo <israelmanzo814@gmail.com>
2025-04-28 22:37:13 -0400

    updated view and model

--- Design Pattern/MVI.playground/Contents.swift
+++ Design Pattern/MVI.playground/Contents.swift
@@ -1,4 +1,4 @@
-import UIKit
+import SwiftUI
 
 /**
  Model-View-Intent (MVI) is a popular architecture in the Android world. It was introduced by Hannes Dorfmann.
@@ -30,3 +30,94 @@ import UIKit
  `- ApiError: The HTTP request failed or the API returned an error
  We will use Swift’s `Enum` to hold the states.
  */
+enum SearchPageState {
+    case Init
+    case Loading(String) // String is the message to be shown while laoding
+    case SuccessfullyFetched([Movie]) // List of matching Movies
+    case NoResultsFound
+    case ApiError(String) // Error message to be shown
+}
+/**
+ `SearchPageViewModel(Business layer)
+ - It contains the State of the Application.
+ - Receives the Intent from the View and updates the State.
+ - Publishes the updated State to the View.
+ */
+class SearchPageViewModel : ObservableObject {
+    
+    @Published var uiState: SearchPageState = .Init
+    
+    let repository: MovieRepository = MovieRepository()
+    
+    func loadMovies(query: String) {
+        
+        // 1. state is changed to Loading
+        self.uiState = .Loading("Querying for \(query)")
+        repository
+            .searchMovies(query: query)
+            .subscribe(
+                onNext: { [weak self] response in
+                    
+                    if response.results.count == 0 {
+                        // 2. State is updated to NoResultsFound
+                        self?.uiState = .NoResultsFound
+                    } else {
+                        // 3. state is updated to SuccesffullyFetched
+                        self?.uiState = .Fetched(response)
+                    }
+                    
+                },
+                onError: { error in
+                    // 4. state is updated to ApiError
+                    self.uiState = .ApiError("Results couldnot be fetched")
+            }
+        )
+    }
+}
+
+/**
+ `View
+ - We display a TextField to enter the keyword. Below that, we have a group that shows the view as per the state of the application.
+ */
+struct SearchPageView: View {
+    @ObservedObject var vm: SearchPageViewModel
+    
+    @State private var query: String = ""
+    
+    init(viewModel: SearchPageViewModel) {
+        self.vm = viewModel
+    }
+    
+    
+    var body: some View {
+        ScrollView {
+            // Enter the keyword here and when commited fire an Intent to load the movies
+            TextField("search movies", text: $query, onCommit: {
+                // Fires an Intent to load the movies for the keyword
+                self.vm.loadMovies(query: self.query)
+            })
+            /**
+            * It contains the exhaustive cases. It observes the uiState and
+            * updates it's view accordingly
+            */
+            Group { () -> AnyView in
+                
+                switch vm.uiState {
+                
+                case .Init:
+                    return AnyView(Text("Please type in to query"))
+                case .Loading(let message):
+                    return AnyView(Text(message))
+                case .SuccessfullyFetched(let movies):
+                    // This displays the list of Movies  matching the keyword
+                    return AnyView(SearchedMoviesView(movies: movies))
+                case .NoResultsFound:
+                    return AnyView(Text("No matching movies found"))
+                    
+                case .ApiError(let errorMessage):
+                    return AnyView(Text(errorMessage))
+                }
+            }
+        }
+    }
+}
