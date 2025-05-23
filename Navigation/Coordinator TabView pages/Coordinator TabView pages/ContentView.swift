8609a23bf81fed3f59765abdf137e37ec6728342
Israel Manzo <israelmanzo814@gmail.com>
2025-05-22 22:21:11 -0400

    added new project for coordinator tabview

---
+++ Navigation/Coordinator TabView pages/Coordinator TabView pages/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coordinator TabView pages
+//
+//  Created by Israel Manzo on 5/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation/Coordinator TabView pages/Coordinator TabView pages/Coordinator_TabView_pagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator_TabView_pagesApp.swift
+//  Coordinator TabView pages
+//
+//  Created by Israel Manzo on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Coordinator_TabView_pagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
