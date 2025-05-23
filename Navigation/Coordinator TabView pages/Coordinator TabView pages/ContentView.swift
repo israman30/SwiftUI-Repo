379eda6c0b3a086153e4cb95e6655bbae7fea8d9
Israel Manzo <israelmanzo814@gmail.com>
2025-05-22 22:40:44 -0400

    added routes

--- Navigation/Coordinator TabView pages/Coordinator TabView pages/ContentView.swift
+++ Navigation/Coordinator TabView pages/Coordinator TabView pages/ContentView.swift
@@ -7,6 +7,62 @@
 
 import SwiftUI
 
+// MARK: - Model
+struct Vendor: Hashable {
+    let name: String
+}
+
+// MARK: - Routes
+enum VendorRoutes: Hashable {
+    case list
+    case create
+    case detail(Vendor)
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+        case .list:
+            Text("Vendor List")
+        case .create:
+            Text("Create Vendor")
+        case .detail(let vendor):
+            Text("Detail of \(vendor.name)")
+        }
+    }
+}
+
+enum CusomterRoutes: Hashable {
+    case list
+    case create
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+        case .list:
+            Text("List")
+        case .create:
+            Text("Create")
+        }
+    }
+}
+
+enum Route: Hashable {
+    case vendor(VendorRoutes)
+    case customer(CusomterRoutes)
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+        case .vendor(let vendorRoutes):
+            vendorRoutes.destination
+        case .customer(let customerRoutes):
+            customerRoutes.destination
+        }
+    }
+}
+
+
+// MARK: - Views
 struct VendorView: View {
     var body: some View {
         Text("Vendor View")
