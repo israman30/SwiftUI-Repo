0f52c440e280ee8c2afcd74d6230263960bc2200
Israel Manzo <israelmanzo814@gmail.com>
2025-05-24 23:03:20 -0400

    added typealias

--- Timer/Timer modifier.playground/Contents.swift
+++ Timer/Timer modifier.playground/Contents.swift
@@ -2,10 +2,13 @@ import SwiftUI
 import Combine
 
 struct TimerModifier: ViewModifier {
-    @State private var timer: Publishers.Autoconnect<Timer.TimerPublisher>
-    private let perform: (Date) -> Void
     
-    init(every interval: TimeInterval, tolerance: TimeInterval?, perform: @escaping (Date) -> Void) {
+    typealias PerformanceClosure = (Date) -> Void
+    
+    @State private var timer: Publishers.Autoconnect<Timer.TimerPublisher>
+    private let perform: PerformanceClosure
+    
+    init(every interval: TimeInterval, tolerance: TimeInterval?, perform: @escaping PerformanceClosure) {
         self.timer = Timer.publish(every: interval, tolerance: tolerance, on: .main, in: .common).autoconnect()
         self.perform = perform
     }
@@ -19,7 +22,9 @@ struct TimerModifier: ViewModifier {
 }
 
 extension View {
-    func onTimer(every interval: TimeInterval, tolerance: TimeInterval? = nil, perform: @escaping (Date) -> Void) {
+    typealias PerformanceClosure = (Date) -> Void
+    
+    func onTimer(every interval: TimeInterval, tolerance: TimeInterval? = nil, perform: @escaping PerformanceClosure) {
         self.modifier(
             TimerModifier(every: interval, tolerance: tolerance, perform: perform)
         )
