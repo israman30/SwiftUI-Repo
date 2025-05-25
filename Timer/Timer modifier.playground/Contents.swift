47d4f0b51fd266d9925266a5a1481a0778df5e69
Israel Manzo <israelmanzo814@gmail.com>
2025-05-24 22:58:07 -0400

    update timer folder + added new timer file

---
+++ Timer/Timer modifier.playground/Contents.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import Combine
+
+struct TimerModifier: ViewModifier {
+    @State private var timer: Publishers.Autoconnect<Timer.TimerPublisher>
+    private let perform: (Date) -> Void
+    
+    init(every interval: TimeInterval, tolerance: TimeInterval?, perform: @escaping (Date) -> Void) {
+        self.timer = Timer.publish(every: interval, tolerance: tolerance, on: .main, in: .common).autoconnect()
+        self.perform = perform
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .onReceive(timer) { date in
+                self.perform(date)
+            }
+    }
+}
