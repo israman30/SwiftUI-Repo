1596d403249d5981b3ca5b08a94bfcefab6e436c
Israel Manzo <israelmanzo814@gmail.com>
2025-05-24 23:12:10 -0400

    update cancel trigger

--- Timer/Timer modifier.playground/Contents.swift
+++ Timer/Timer modifier.playground/Contents.swift
@@ -6,10 +6,12 @@ struct TimerModifier: ViewModifier {
     typealias PerformanceClosure = (Date) -> Void
     
     @State private var timer: Publishers.Autoconnect<Timer.TimerPublisher>
+    let cancelTrigger: AnyHashable?
     private let perform: PerformanceClosure
     
-    init(every interval: TimeInterval, tolerance: TimeInterval?, perform: @escaping PerformanceClosure) {
+    init(every interval: TimeInterval, tolerance: TimeInterval?, cancelTrigger: AnyHashable?, perform: @escaping PerformanceClosure) {
         self.timer = Timer.publish(every: interval, tolerance: tolerance, on: .main, in: .common).autoconnect()
+        self.cancelTrigger = cancelTrigger
         self.perform = perform
     }
     
@@ -18,15 +20,26 @@ struct TimerModifier: ViewModifier {
             .onReceive(timer) { date in
                 self.perform(date)
             }
+            .onChange(of: cancelTrigger) { _, _ in
+                self.timer.upstream.connect().cancel()
+            }
     }
 }
 
 extension View {
     typealias PerformanceClosure = (Date) -> Void
     
-    func onTimer(every interval: TimeInterval, tolerance: TimeInterval? = nil, perform: @escaping PerformanceClosure) {
+    func onTimer(every interval: TimeInterval, tolerance: TimeInterval? = nil, cancelTrigger: AnyHashable? = nil, perform: @escaping PerformanceClosure) {
         self.modifier(
-            TimerModifier(every: interval, tolerance: tolerance, perform: perform)
+            TimerModifier(every: interval, tolerance: tolerance, cancelTrigger: cancelTrigger, perform: perform)
         )
     }
 }
+
+// usage
+var currentDate = Date()
+
+Text(Date.now.formatted(date: .numeric, time: .standard))
+    .onTimer(every: 1, tolerance: 0.05) { date in
+        currentDate = date
+    }
