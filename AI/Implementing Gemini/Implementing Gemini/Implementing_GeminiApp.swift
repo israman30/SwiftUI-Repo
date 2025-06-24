5eb04e6d5aa2a1595bd92d91d241b6171f3ba0e2
Israel Manzo <israelmanzo814@gmail.com>
2025-06-24 15:52:44 -0400

    added new project for AI

---
+++ AI/Implementing Gemini/Implementing Gemini/Implementing_GeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Implementing_GeminiApp.swift
+//  Implementing Gemini
+//
+//  Created by Israel Manzo on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Implementing_GeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
