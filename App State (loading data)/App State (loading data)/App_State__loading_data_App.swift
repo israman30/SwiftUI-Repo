8f0d0c8451e6fbc01fdcd039a2f1fd1132e35f9b
Israel Manzo <israelmanzo814@gmail.com>
2025-07-16 13:58:03 -0400

    Added new project for app state

---
+++ App State (loading data)/App State (loading data)/App_State__loading_data_App.swift
@@ -0,0 +1,17 @@
+//
+//  App_State__loading_data_App.swift
+//  App State (loading data)
+//
+//  Created by Israel Manzo on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct App_State__loading_data_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App State (loading data)/App State (loading data)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App State (loading data)
+//
+//  Created by Israel Manzo on 7/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
