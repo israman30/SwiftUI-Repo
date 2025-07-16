e90c4a93d844073f1acfa1787c5437844e050cd6
Israel Manzo <israelmanzo814@gmail.com>
2025-07-16 16:34:01 -0400

    display item in the a list

--- App State (loading data)/App State (loading data)/ContentView.swift
+++ App State (loading data)/App State (loading data)/ContentView.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
+import Observation
+
+@Observable
+class StateViewModel {
+    var data = [String]()
+    
+    func fetchData() async {
+        do {
+            try await Task.sleep(for: .seconds(2))
+            data = Array(1...100).map(\.description)
+        } catch {
+            print("Some error occurred: \(error)")
+        }
+    }
+}
 
 struct ContentView: View {
+    @State private var viewModel = StateViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(viewModel.data, id: \.self) { data in
+            Text("Item No: \(data)")
         }
-        .padding()
+        .task {
+            await viewModel.fetchData()
+        }
     }
 }
 
