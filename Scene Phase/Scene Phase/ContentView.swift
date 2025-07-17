58a6ccd5fd91ffd379305cc84b9652cd1a2ba702
Israel Manzo <israelmanzo814@gmail.com>
2025-07-16 23:11:15 -0400

    update phase

--- Scene Phase/Scene Phase/ContentView.swift
+++ Scene Phase/Scene Phase/ContentView.swift
@@ -9,14 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @Environment
+    @Environment(\.scenePhase) private var scenePhase
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Scene Phase App")
+                .onChange(of: scenePhase) { oldState, newValue in
+                    if newValue == .inactive {
+                        print(">>> Scene is inactive <<<")
+                    } else if newValue == .active {
+                        print(">>> Scene is active <<<")
+                    } else if newValue == .background {
+                        print(">> Scene is in background")
+                    } else {
+                        print(">> Unknown scene phase: \(newValue)")
+                    }
+                }
         }
         .padding()
     }
