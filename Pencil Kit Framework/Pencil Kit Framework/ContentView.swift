c986e2ffce03b4eb1a90acf436c4651e820d7615
Israel Manzo <israelmanzo814@gmail.com>
2025-08-07 22:52:15 -0400

    replace project for name issue

---
+++ Pencil Kit Framework/Pencil Kit Framework/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Pencil Kit Framework
+//
+//  Created by Israel Manzo on 8/7/25.
+//
+
+import SwiftUI
+import PencilKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pencil Kit Framework/Pencil Kit Framework/Pencil_Kit_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pencil_Kit_FrameworkApp.swift
+//  Pencil Kit Framework
+//
+//  Created by Israel Manzo on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Pencil_Kit_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
