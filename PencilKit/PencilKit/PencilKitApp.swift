2c4b4f96dc0761cb1c1ae0520eee7b2ede8401c6
Israel Manzo <israelmanzo814@gmail.com>
2025-08-07 22:47:03 -0400

    added new project

---
+++ PencilKit/PencilKit/PencilKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitApp.swift
+//  PencilKit
+//
+//  Created by Israel Manzo on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
