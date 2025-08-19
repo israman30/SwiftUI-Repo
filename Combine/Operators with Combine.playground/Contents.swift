015787fd091b07a17fd32837d9833c927456b5c0
Israel Manzo <israelmanzo814@gmail.com>
2025-08-18 22:18:03 -0400

    added some concept

--- Combine/Operators with Combine.playground/Contents.swift
+++ Combine/Operators with Combine.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/**
+ In the earlier parts of this series, we explored `Publishers`, `Subscribers`, and how data flows in Combine. Now, it’s time to dive into one of the most powerful aspects of Combine: Operators.
+
+ Operators are the transformers in Combine’s pipeline. They allow you to modify, filter, combine, and handle data as it travels from `Publisher → Operator(s) → Subscriber`.
+ 
+ `What are Operators in Combine?
+ Operators are methods you apply to a Publisher to transform or react to the emitted values.
+
+ Think of them like the functions in a data pipeline:
+
+ A `map` operator can transform values.
+ A `filter` operator can drop unwanted values.
+ A `debounce` operator can control the frequency of events.
+ 
+ `Categories of Operators
+ Operators in Combine can be broadly divided into:
+
+ `Transforming Operators` → Change the output values.
+ `map`, `compactMap`, `replaceNil`, `scan`, `flatMap`.
+ `Filtering Operators `→ Control which values are allowed to pass.
+ `filter`, `removeDuplicates`, `dropFirst`, `prefix`.
+ `Combining Operators` → Merge or zip multiple publishers.
+ `merge`, `combineLatest`, `zip`, `switchToLatest`.
+ `Time-Based Operators` → Handle delays and throttling.
+ `delay`, `debounce`, `throttle`, `timeout`.
+ `Error-Handling Operators` → Handle failures gracefully.
+ `catch`, `retry`, `replaceError`.
+ */
