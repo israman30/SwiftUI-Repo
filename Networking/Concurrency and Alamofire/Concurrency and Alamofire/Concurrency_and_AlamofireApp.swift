6e4eb31a0d2fb58526d384ed6d863c7ecbf86af0
Israel Manzo <israelmanzo814@gmail.com>
2025-08-26 22:36:51 -0400

    added new project for alamofire and concurrency

---
+++ Networking/Concurrency and Alamofire/Concurrency and Alamofire/Concurrency_and_AlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concurrency_and_AlamofireApp.swift
+//  Concurrency and Alamofire
+//
+//  Created by Israel Manzo on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Concurrency_and_AlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Networking/Concurrency and Alamofire/Concurrency and Alamofire/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Concurrency and Alamofire
+//
+//  Created by Israel Manzo on 8/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
