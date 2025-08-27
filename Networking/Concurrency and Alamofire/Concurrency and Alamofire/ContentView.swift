a1d0a471d34e175cd16d8f267465e50eef2b26d9
Israel Manzo <israelmanzo814@gmail.com>
2025-08-26 22:45:37 -0400

    adde AF network layer

--- Networking/Concurrency and Alamofire/Concurrency and Alamofire/ContentView.swift
+++ Networking/Concurrency and Alamofire/Concurrency and Alamofire/ContentView.swift
@@ -6,6 +6,34 @@
 //
 
 import SwiftUI
+import Alamofire
+
+struct User: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let email: String
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    func fetchUsers() async throws -> [User] {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+        let data = try Data(contentsOf: url)
+        return try await withCheckedThrowingContinuation { continuation in
+            AF.request(url)
+                .validate()
+                .responseDecodable(of: [User].self) { response in
+                    switch response.result {
+                    case .success(let users):
+                        continuation.resume(returning: users)
+                    case .failure(let error):
+                        continuation.resume(throwing: error)
+                    }
+                }
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
