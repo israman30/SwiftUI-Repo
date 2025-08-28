4caf7d59b83feb410d17919c733005af6dda8fa5
Israel Manzo <israelmanzo814@gmail.com>
2025-08-27 22:45:45 -0400

    added function for adding keyt

--- Keychain/Swift Keychain .playground/Contents.swift
+++ Keychain/Swift Keychain .playground/Contents.swift
@@ -1,3 +1,50 @@
 import UIKit
+import Security
 
-var greeting = "Hello, playground"
+/**
+ `Keychain services handles data encryption and storage (including data attributes) in a keychain, which is an encrypted database stored on disk. Later, authorized apps use keychain services to find the item and decrypt its data.
+ 
+ If you have created the cryptographic key with `SecKeyCreateRandomKey(_:_:)` and specify the `kSecAttrIsPermanent` in the attributes dictionary to true, it will be stored in the default keychain automatically while creating!
+ */
+final class KeychainService {
+    enum KeychainError: Error {
+        case saveError(OSStatus)
+        case retrieveError(OSStatus)
+        case failToExtractInfo
+        case deleteError(OSStatus)
+    }
+    
+    private let services = "com.some.service.name"
+    
+    func saveGenericPassword(password: String = "@12345", account: String = "someaccount") async throws {
+        let passwordData = Data(password.utf8)
+        
+        // For a full list of attribute keys supported for a generic password item:
+        // https://developer.apple.com/documentation/security/ksecclassgenericpassword
+        var query = makePrimaryDictionary(for: account)
+        query[kSecValueData as String] = passwordData
+        
+        let status = SecItemAdd(query as CFDictionary, nil)
+        
+        if status != errSecSuccess {
+            throw KeychainError.saveError(status)
+        }
+    }
+    
+    private func makePrimaryDictionary(for account: String) -> [String: Any] {
+        return [
+            // the class of the key
+            kSecClass as String: kSecClassGenericPassword,
+            // attributes form the composite primary keys of a generic password item
+            kSecAttrAccount as String: account,
+            kSecAttrService as String: services
+        ]
+    }
+}
+
+extension OSStatus {
+    var string: String {
+        let string = SecCopyErrorMessageString(self, nil)
+        return string as? String ?? "code: \(self)"
+    }
+}
