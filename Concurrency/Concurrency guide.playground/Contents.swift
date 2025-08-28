57555c24b9a5789de5f88af6835cc7985efa8ca2
Israel Manzo <israelmanzo814@gmail.com>
2025-08-27 23:08:28 -0400

    adding new folder for concurrency

---
+++ Concurrency/Concurrency guide.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+/**
+ `Concurrency is an essential concept in modern app development. Mobile apps often perform multiple tasks simultaneously — fetching data from APIs, processing images, updating UI, and handling user interactions. If not managed properly, these tasks can block the main thread, leading to a poor user experience.
+
+ `To solve this, Apple introduced the Swift Concurrency model in Swift 5.5 (iOS 15+), which brings structured concurrency using async/await, Tasks, and Actors.
+ 
+ `- Downloading images in the background.
+ `- Fetching data from a REST API while showing a loading indicator.
+ `- Processing files while letting users interact with UI.
+ */
