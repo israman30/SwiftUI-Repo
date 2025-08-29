e97c8e6814e114116807d6971ddecbef4ff34197
Israel Manzo <israelmanzo814@gmail.com>
2025-08-29 12:13:06 -0400

    update segmented view

--- Segmented Control/Segmented Control/ContentView.swift
+++ Segmented Control/Segmented Control/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-enum Segement: String, CaseIterable, Identifiable {
+enum Segment: String, CaseIterable, Identifiable {
     case swift
     case uikit
     case swiftUI
@@ -16,15 +16,35 @@ enum Segement: String, CaseIterable, Identifiable {
 }
 
 struct ContentView: View {
+    @State private var selectedSegment: Segment = .swift
+    @Namespace var nameSpace
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            ForEach(Segment.allCases, id: \.id) { segment in
+                Button {
+                    withAnimation(.easeInOut) {
+                        selectedSegment = segment
+                    }
+                } label: {
+                    Text(segment.rawValue.capitalized)
+                        .bold()
+                        .foregroundStyle(Color.primary)
+                        .frame(maxWidth: .infinity)
+                        .padding(15)
+                }
+                .matchedGeometryEffect(id: segment.id, in: nameSpace)
+                .padding(2)
+            }
         }
-        .padding()
+        .background {
+            Capsule(style: .continuous)
+                .matchedGeometryEffect(id: selectedSegment.id, in: nameSpace, isSource: false)
+                .foregroundStyle(Color.blue.gradient)
+                .shadow(color: .black.opacity(0.2), radius: 2)
+        }
+        .background(Color.blue.opacity(0.2))
+        .clipShape(Capsule())
     }
 }
 
