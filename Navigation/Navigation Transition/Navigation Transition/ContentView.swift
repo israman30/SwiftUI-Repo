e8931c78d4e0591f6c5544cc07b93726877d0689
Israel Manzo <israelmanzo814@gmail.com>
2025-08-29 22:00:53 -0400

    added new project for navigation

---
+++ Navigation/Navigation Transition/Navigation Transition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigation Transition
+//
+//  Created by Israel Manzo on 8/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
