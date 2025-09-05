4435f668dab7609a8b0d3886d154f1f40558a01f
Israel Manzo <israelmanzo814@gmail.com>
2025-09-05 09:07:40 -0400

    added new keychain playground

---
+++ Keychain/Keychain 2.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Security
+
+struct Keychain<T: Codable> {
+    static func set(_ value: T, forKey: String) {
+        do {
+            
+        } catch {
+            
+        }
+    }
+}
