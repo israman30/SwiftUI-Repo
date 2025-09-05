fe4a7d2571b7749a6572cc69fae1249fbe020878
Israel Manzo <israelmanzo814@gmail.com>
2025-09-05 09:11:43 -0400

    added set keyichain

--- Keychain/Keychain 2.playground/Contents.swift
+++ Keychain/Keychain 2.playground/Contents.swift
@@ -4,9 +4,21 @@ import Security
 struct Keychain<T: Codable> {
     static func set(_ value: T, forKey: String) {
         do {
+            let data = try JSONEncoder().encode(value)
+            let query: [CFString:Any] = [
+                kSecClass: kSecClassGenericPassword,
+                kSecAttrAccount: forKey,
+                kSecValueData: data
+            ]
+            SecItemDelete(query as CFDictionary) // delete existing data
             
+            let status = SecItemAdd(query as CFDictionary, nil)
+            
+            if status != errSecSuccess {
+                print("Error loading item keychain: \(data)")
+            }
         } catch {
-            
+            print("Error encoding data: \(error)")
         }
     }
 }
