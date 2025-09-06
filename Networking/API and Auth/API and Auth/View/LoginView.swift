1fe0fd5c0db23f349350d76e753559bba2a7551b
Israel Manzo <israelmanzo814@gmail.com>
2025-09-05 23:33:03 -0400

    adding signup string components

--- Networking/API and Auth/API and Auth/View/LoginView.swift
+++ Networking/API and Auth/API and Auth/View/LoginView.swift
@@ -22,8 +22,40 @@ struct LoginView: View {
             }
         }
     }
+    
+    private func valdidate() -> [String] {
+        var errors = [String]()
+        
+        if name.isEmptyOrWhitespace {
+            errors.append("Name is required")
+        } else if email.isEmptyOrWhitespace {
+            errors.append("Email is required")
+        } else if password.isEmptyOrWhitespace {
+            errors.append("Password is required")
+        } else {
+            errors.append("All fields are required")
+        }
+        
+        
+        return errors
+    }
 }
 
 #Preview {
     LoginView()
+}
+
+extension String {
+    var isEmptyOrWhitespace: Bool {
+        trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
+    
+    var isValidpassword: Bool {
+        count >= 8
+    }
+    
+    var isEmailValid: Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        return range(of: emailRegex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
 }
