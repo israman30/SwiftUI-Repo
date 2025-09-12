4ed5d5901b75b980c51ef58ae360c61dd53305fe
Israel Manzo <israelmanzo814@gmail.com>
2025-09-12 18:37:12 -0400

    added container presenter pattern

--- Architecture/Container Presenting Pattern/Container Presenting Pattern/ContentView.swift
+++ Architecture/Container Presenting Pattern/Container Presenting Pattern/ContentView.swift
@@ -11,16 +11,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        UserListScreen()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+// Model
+struct User: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
+
+// Network
+struct NetworkClient {
+    func fetchUsers() async throws -> [User] {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([User].self, from: data)
+    }
+}
+
+extension EnvironmentValues {
+    @Entry var networkClient: NetworkClient = .init()
+}
+
+// Presenter
+struct UserListView: View {
+    let users: [User]
+    var body: some View {
+        List(users) { user in
+            Text(user.name)
+        }
+    }
+}
+
+// Container
+struct UserListScreen: View {
+    
+    @Environment(\.networkClient) private var networkClient
+    @State private var users: [User] = []
+    
+    var body: some View {
+        VStack {
+            UserListView(users: users)
+                .task {
+                    do {
+                        users = try await networkClient.fetchUsers()
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+        }
+    }
 }
