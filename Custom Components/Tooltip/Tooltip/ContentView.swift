3b1385ad701d3ad73f62d999c863bfab7ab83d7d
Israel Manzo <israelmanzo814@gmail.com>
2025-09-13 14:47:31 -0400

    added new project for tooltip

---
+++ Custom Components/Tooltip/Tooltip/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tooltip
+//
+//  Created by Israel Manzo on 9/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct
---
+++ Custom Components/Tooltip/Tooltip/TooltipApp.swift
@@ -0,0 +1,17 @@
+//
+//  TooltipApp.swift
+//  Tooltip
+//
+//  Created by Israel Manzo on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TooltipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
