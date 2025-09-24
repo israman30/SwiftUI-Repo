def4f8a8054c513d542e0d756b8587d1451416b8
Israel Manzo <israelmanzo814@gmail.com>
2025-09-23 21:45:16 -0400

    added new property

--- Custom Components/Container Relative Frame/Container Relative Frame/ContentView.swift
+++ Custom Components/Container Relative Frame/Container Relative Frame/ContentView.swift
@@ -23,10 +23,10 @@ struct ContainerRelativeFrameDemo: View {
     var body: some View {
         ScrollView(.vertical) {
             VStack(spacing: 8) {
-                ForEach(0..<10, id: \.self) { _ in
+                ForEach(0..<10, id: \.self) { index in
                     RoundedRectangle(cornerRadius: 8)
                         .fill(.blue.opacity(0.8))
-                        .containerRelativeFrame([.vertical], count: 3, spacing: 24)
+                        .containerRelativeFrame([.vertical], count: 3, span: index % 2 + 1, spacing: 24)
                 }
             }
             .padding()
