19e388af964c756da1636c608daee31058df15c7
Israel Manzo <israelmanzo814@gmail.com>
2025-09-23 21:52:41 -0400

    update second demo

--- Custom Components/Container Relative Frame/Container Relative Frame/ContentView.swift
+++ Custom Components/Container Relative Frame/Container Relative Frame/ContentView.swift
@@ -40,9 +40,25 @@ struct ContainerRelativeFrameDemo2: View {
         ScrollView(.vertical) {
             VStack(spacing: 8) {
                 ForEach(0..<10, id: \.self) { index in
+                    let alignment: (Alignment, String) = switch index % 3 {
+                    case 0:
+                        (.top, "top")
+                    case 1:
+                        (.center, "center")
+                    default:
+                        (.bottom, "bottom")
+                    }
+                    Text(alignment.1)
+                        .containerRelativeFrame([.vertical], alignment: .center) { length, axis in
+                            if axis == .horizontal {
+                                return length * 0.6
+                            } else {
+                                return length * 0.5
+                            }
+                        }
                     RoundedRectangle(cornerRadius: 8)
-                        .fill(.blue.opacity(0.8))
-                        .containerRelativeFrame([.vertical], count: 3, spacing: 24)
+                        .fill(.black.opacity(0.8))
+                        .frame(height: 2)
                 }
             }
             .padding()
