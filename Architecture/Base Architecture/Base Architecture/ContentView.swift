6d4987d848058aa4d81ef42acec32598d6cce84d
Israel Manzo <israelmanzo814@gmail.com>
2025-10-01 17:17:28 -0400

    added network layer

--- Architecture/Base Architecture/Base Architecture/ContentView.swift
+++ Architecture/Base Architecture/Base Architecture/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+struct User: Decodable {
+    var id: Int
+    var name: String
+    var email: String
+}
+
+final class NetworkManager {
+    func fetch() async throws -> [User] {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([User].self, from: data)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
