ba46468d3db937ec29a850f7625c0ce8873f42f0
Israel Manzo <israelmanzo814@gmail.com>
2025-10-01 22:08:44 -0400

    update view model

--- Architecture/Base Architecture/Base Architecture/ContentView.swift
+++ Architecture/Base Architecture/Base Architecture/ContentView.swift
@@ -29,12 +29,32 @@ extension EnvironmentValues {
 final class UserViewModel: ObservableObject {
     @Environment(\.api) private var api
     @Published var users: [User] = []
+    @Published var loadingState: LoadingState = .loading
+    
+    enum LoadingState {
+        case loading
+        case success([User])
+        case error(Error)
+    }
     
     func load() async {
         do {
             users = try await api.fetch()
         } catch {
-            print(error.localizedDescription)
+            loadingState = .error(error)
+        }
+    }
+}
+
+struct UserListView: View {
+    let users: [User]
+    var body: some View {
+        if users.isEmpty {
+            ContentUnavailableView("No user", image: "xmark.circle")
+        } else {
+            List(users, id: \.id) { user in
+                Text(user.name)
+            }
         }
     }
 }
@@ -44,11 +64,17 @@ struct ContentView: View {
     @StateObject private var viewModel = UserViewModel()
     
     var body: some View {
-        List(viewModel.users, id: \.id) { user in
-            Text(user.name)
-        }
-        .task {
-            await viewModel.load()
+        switch viewModel.loadingState {
+        case .loading:
+            ProgressView("Loading...")
+                .task {
+                    await viewModel.load()
+                    viewModel.loadingState = .success(viewModel.users)
+                }
+        case .success(let users):
+            UserListView(users: users)
+        case .error(let error):
+            Text(error.localizedDescription)
         }
     }
 }
