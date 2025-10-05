fe6523c7ec63062eba195946475f557b8000aedb
Israel Manzo <israelmanzo814@gmail.com>
2025-10-04 23:08:03 -0400

    added new project

---
+++ Custom Components/Container Frame/Container Frame/Container_FrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Container_FrameApp.swift
+//  Container Frame
+//
+//  Created by Israel Manzo on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Container_FrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Custom Components/Container Frame/Container Frame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Container Frame
+//
+//  Created by Israel Manzo on 10/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Rectangle()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
