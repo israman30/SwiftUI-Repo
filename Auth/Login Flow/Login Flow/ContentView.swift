5cb5db919bab64d43f5c3b3c004230339be8ec39
Israel Manzo <israelmanzo814@gmail.com>
2026-05-05 10:03:46 -0400

    add new project for login flow

---
+++ Auth/Login Flow/Login Flow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Login Flow
+//
+//  Created by Israel Manzo on 5/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Auth/Login Flow/Login Flow/Login_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_FlowApp.swift
+//  Login Flow
+//
+//  Created by Israel Manzo on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Login_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
