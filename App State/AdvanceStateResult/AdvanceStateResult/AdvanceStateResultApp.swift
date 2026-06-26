29212ee9352a832771139be680b6492f71c2cc36
Israel Manzo <israelmanzo814@gmail.com>
2026-06-26 10:40:14 -0400

    add new advance state app

---
+++ App State/AdvanceStateResult/AdvanceStateResult/AdvanceStateResultApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvanceStateResultApp.swift
+//  AdvanceStateResult
+//
+//  Created by Israel Manzo on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct AdvanceStateResultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
