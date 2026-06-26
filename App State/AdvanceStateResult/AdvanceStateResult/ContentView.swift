cbd6d59508baeb90b588e354d19868e3899c15c2
Israel Manzo <israelmanzo814@gmail.com>
2026-06-26 11:02:24 -0400

    add view model and network call

--- App State/AdvanceStateResult/AdvanceStateResult/ContentView.swift
+++ App State/AdvanceStateResult/AdvanceStateResult/ContentView.swift
@@ -7,6 +7,42 @@
 
 import SwiftUI
 
+struct User: Decodable {
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class NetworkManager {
+    func fetchData() async throws -> [User] {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+        let (data, _ ) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([User].self, from: data)
+    }
+}
+
+@MainActor
+@Observable
+class UserViewModel {
+    var appState: AppState<[User], Error> = .idle
+    private let network: NetworkManager
+    
+    init(network: NetworkManager) {
+        self.network = network
+    }
+    
+    func loadData() async {
+        appState.startLoading()
+        
+        do {
+            let users = try await network.fetchData()
+            appState = .loaded(users)
+        } catch {
+            appState = .failed(error, previous: appState.value)
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
