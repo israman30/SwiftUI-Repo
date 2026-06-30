be1aecbc340bc5daac1c08ea6988e6ac8185e335
Israel Manzo <israelmanzo814@gmail.com>
2026-06-30 10:12:39 -0400

    added new app state project

---
+++ App State/AppStateTuple/AppStateTuple/AppStateTupleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStateTupleApp.swift
+//  AppStateTuple
+//
+//  Created by Israel Manzo on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct AppStateTupleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App State/AppStateTuple/AppStateTuple/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  AppStateTuple
+//
+//  Created by Israel Manzo on 6/30/26.
+//
+
+import SwiftUI
+
+enum AppState: Equatable {
+    case loading
+    case empty
+    case loaded
+    case error(Error)
+    
+    static func == (lhs: AppState, rhs: AppState) -> Bool {
+        switch (lhs, rhs) {
+        case (.loading, .loading):
+            return true
+        case (.empty, .empty):
+            return true
+        case (.loaded, .loaded):
+            return true
+        case (.error, .error):
+            return true
+        default:
+            return false
+        }
+    }
+    
+}
+
+
+
+struct User: Decodable {
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class NetworkManager {
+    func fetchData() async throws -> [User] {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+        let (data, _ ) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([User].self, from: data)
+    }
+}
+
+@Observable
+@MainActor
+class PostViewModel {
+    
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
