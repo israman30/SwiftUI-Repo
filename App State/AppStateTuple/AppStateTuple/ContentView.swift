d6a8fe7ed55cedcb16a2996f3c20466040dc725c
Israel Manzo <israelmanzo814@gmail.com>
2026-06-30 10:14:18 -0400

    add extension

--- App State/AppStateTuple/AppStateTuple/ContentView.swift
+++ App State/AppStateTuple/AppStateTuple/ContentView.swift
@@ -27,7 +27,28 @@ enum AppState: Equatable {
             return false
         }
     }
+}
+
+extension AppState {
+    var isLoading: Bool {
+        if case .loading = self { return true }
+        return false
+    }
     
+    var isEmpty: Bool {
+        if case .empty = self { return true }
+        return false
+    }
+    
+    var isLoaded: Bool {
+        if case .loaded = self { return true }
+        return false
+    }
+    
+    var error: Error? {
+        if case .error(let error) = self { return error }
+        return nil
+    }
 }
 
 
