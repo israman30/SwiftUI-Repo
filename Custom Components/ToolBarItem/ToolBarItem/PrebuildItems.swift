494c1a32c74849d1967168a5dd6304ab8c4180ee
Israel Manzo <israelmanzo814@gmail.com>
2026-07-02 12:37:42 -0400

    add prebuild items - back button

---
+++ Custom Components/ToolBarItem/ToolBarItem/PrebuildItems.swift
@@ -0,0 +1,18 @@
+//
+//  PrebuildItems.swift
+//  ToolBarItem
+//
+//  Created by Israel Manzo on 7/2/26.
+//
+
+import SwiftUI
+
+// MARK: - Common Toolbar Items
+struct ToolbarItems {
+    // MARK: - Back Button
+    static func backButton(action: @escaping () -> Void) -> some ToolbarContent {
+        ToolBarButton(placement: .navigationBarLeading, icon: "chevron.left", label: "Back", action: action)
+    }
+    
+    
+}
