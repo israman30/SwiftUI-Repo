ee16f37f7e7792943189b2ba698f0d3b25e6cfce
Israel Manzo <israelmanzo814@gmail.com>
2026-07-20 14:03:34 -0400

    added reusability

---
+++ Navigation/Coordinator Dynamic/Coordinator Dynamic/ResuableCoordinator.swift
@@ -0,0 +1,54 @@
+//
+//  ResuableCoordinator.swift
+//  Coordinator Dynamic
+//
+//  Created by Israel Manzo on 7/20/26.
+//
+
+import SwiftUI
+import Combine
+
+protocol NavigationRoute: Hashable {
+    associatedtype Content: View
+    
+    @ViewBuilder
+    func build() -> Content
+}
+
+@MainActor
+class ReusableCoordinator<Route: NavigationRoute>: ObservableObject {
+    @Published var path = NavigationPath()
+    
+    func push(_ route: Route) {
+        path.append(route)
+    }
+    
+    func pop() {
+        guard !path.isEmpty else { return }
+        path.removeLast()
+    }
+    
+    func replace(with route: Route) {
+        pop()
+        push(route)
+    }
+    
+    var canPop: Bool {
+        !path.isEmpty
+    }
+}
+
+struct CoordinatorBuilder<Route: NavigationRoute>: View {
+    @ObservedObject var coordinator: ReusableCoordinator<Route>
+    let route: Route
+    
+    var body: some View {
+        NavigationStack(path: $coordinator.path) {
+            route.build()
+                .navigationDestination(for: Route.self) { route in
+                    route.build()
+                }
+        }
+        .environmentObject(coordinator)
+    }
+}
---
+++ Navigation/Coordinator Dynamic/Coordinator Dynamic/ReusableCoordinatorSampleView.swift
@@ -0,0 +1,65 @@
+//
+//  ReusableCoordinatorSampleView.swift
+//  Coordinator Dynamic
+//
+//  Created by Israel Manzo on 7/20/26.
+//
+
+import SwiftUI
+
+enum AppPages: NavigationRoute {
+    case home
+    case detail(user: User)
+    
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case .home:
+            hasher.combine("home")
+        case .detail(let user):
+            hasher.combine("detail")
+            hasher.combine(user.id)
+        }
+    }
+    
+    static func == (lhs: AppPages, rhs: AppPages) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
+    
+    @ViewBuilder
+    func build() -> some View {
+        switch self {
+        case .home:
+            ReusableCoordinatorSampleView()
+        case .detail(let user):
+            DetailView(user: user)
+        }
+    }
+}
+
+typealias AppCoordinator = ReusableCoordinator<AppPages>
+typealias AppCoordinatorView = CoordinatorBuilder<AppPages>
+
+struct ReusableCoordinatorSampleView: View {
+    @EnvironmentObject var coordinator: AppCoordinator
+    @StateObject var vm = UserViewModel(network: NetworkLayer())
+
+    var body: some View {
+        List(vm.users) { user in
+            Button {
+                coordinator.push(.detail(user: user))
+            } label: {
+                DetailView(user: user)
+            }
+        }
+        .navigationTitle("Usage")
+        .task {
+            await vm.loadUsers()
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ReusableCoordinatorSampleView()
+    }.environmentObject(Coordinator())
+}
