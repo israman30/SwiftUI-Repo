dab316d7a2dc129712c7991e338cb3672e39fa9c
Israel Manzo <israelmanzo814@gmail.com>
2026-07-19 23:05:43 -0400

    add a new coordinator

---
+++ Navigation/Coordinator Dynamic/Coordinator Dynamic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coordinator Dynamic
+//
+//  Created by Israel Manzo on 7/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation/Coordinator Dynamic/Coordinator Dynamic/Coordinator_DynamicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinator_DynamicApp.swift
+//  Coordinator Dynamic
+//
+//  Created by Israel Manzo on 7/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Coordinator_DynamicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
