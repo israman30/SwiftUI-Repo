8af9e6ea8244c41ffcc9d4efa75618388e4b7741
Israel Manzo <israelmanzo814@gmail.com>
2026-07-20 16:44:05 -0400

    add documentation

--- Navigation/Coordinator Dynamic/Coordinator Dynamic/ModuleUsageView.swift
+++ Navigation/Coordinator Dynamic/Coordinator Dynamic/ModuleUsageView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 import Navigation
 
+/**
+ If `User` is a custom struct or enum you created, the easiest fix is to attach : `Equatable` directly to its definition. Swift will automatically synthesize the underlying equality check for `User` if all of its internal properties (like String or Int) are equatable.
+ ```
+ // 1. Explicitly conform User to Equatable
+ struct User: Equatable {
+     let id: UUID
+     let name: String
+ }
+
+ // 2. Pages will now automatically synthesize Equatable
+ enum Routes: Equatable {
+     case home
+     case profile(User)
+ }
+ ```
+ // If `User` belongs to an external library or is a protocol/class where you cannot add `Equatable` directly, you must write the `==` comparison logic yourself inside an extension of `Pages`.
+ or use Equatable methods
+ ```
+ func hash(into hasher: inout Hasher) { ... }
+ static func == (lhs: Routes, rhs: Routes) -> Bool { ... }
+
+ ```
+ */
+
 enum RoutePages: NavigationRoute {
     case home
     case detail(user: User)
     
+    /**
+     Must explicitly make your User type conform to the Equatable protocol
+     
+     */
     func hash(into hasher: inout Hasher) {
         switch self {
         case .home:
