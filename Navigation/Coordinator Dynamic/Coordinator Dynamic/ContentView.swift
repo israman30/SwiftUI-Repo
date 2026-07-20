13d80aef54c98656d1530ab55c2815bd1c503f00
Israel Manzo <israelmanzo814@gmail.com>
2026-07-19 23:06:31 -0400

    add coodinator

--- Navigation/Coordinator Dynamic/Coordinator Dynamic/ContentView.swift
+++ Navigation/Coordinator Dynamic/Coordinator Dynamic/ContentView.swift
@@ -6,6 +6,40 @@
 //
 
 import SwiftUI
+import  Combine
+
+enum Pages: Hashable {
+    case home
+    case detail(id: String, title: String, description: String)
+    case settings
+}
+
+@MainActor
+class Coordinator: ObservableObject {
+    @Published var path = NavigationPath()
+    
+    func push(_ page: Pages) {
+        path.append(page)
+    }
+    
+    func pop() {
+        guard !path.isEmpty else { return }
+        path.removeLast()
+    }
+    
+    @ViewBuilder
+    func build(_ page: Pages) -> some View {
+        switch page {
+        case .home:
+            ContentView()
+        case .detail(let id, let title, let description):
+            DetailView(id: id, title: title, description: description)
+        case .settings:
+            EmptyView()
+        }
+    }
+    
+}
 
 struct ContentView: View {
     var body: some View {
@@ -21,4 +55,17 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct DetailView: View {
+    let id: String
+    let title: String
+    let description: String
+    var body: some View {
+        VStack {
+            Text("id: \(id) - \(title)")
+            Text(title)
+            Text(description)
+        }
+    }
 }
