44160c4cb1fe29dfc1ec9c49f5669d359564f2f1
Israel Manzo <israelmanzo814@gmail.com>
2026-07-21 14:50:35 -0400

    update reusable module

--- Navigation/Coordinator Dynamic/Navigation/Navigation.swift
+++ Navigation/Coordinator Dynamic/Navigation/Navigation.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import Combine
 
-public protocol NavigationRoute: Hashable {
+public protocol NavigationRoute: Hashable, Identifiable {
     associatedtype Content: View
     
     @ViewBuilder
@@ -18,6 +18,7 @@ public protocol NavigationRoute: Hashable {
 @MainActor
 public class ReusableCoordinator<Route: NavigationRoute>: ObservableObject {
     @Published var path = NavigationPath()
+    @Published var sheetRoute: Route?
     
     public func push(_ route: Route) {
         path.append(route)
@@ -36,11 +37,25 @@ public class ReusableCoordinator<Route: NavigationRoute>: ObservableObject {
     public var canPop: Bool {
         !path.isEmpty
     }
+    
+    // Sheet Presentation
+    public func presentSheet(_ route: Route) {
+        sheetRoute = route
+    }
+    
+    public func dismissSheet() {
+        sheetRoute = nil
+    }
 }
 
 public struct CoordinatorBuilder<Route: NavigationRoute>: View {
     @ObservedObject var coordinator: ReusableCoordinator<Route>
     let route: Route
+    
+    public init(coordinator: ReusableCoordinator<Route>, route: Route) {
+        self.coordinator = coordinator
+        self.route = route
+    }
     
     public var body: some View {
         NavigationStack(path: $coordinator.path) {
@@ -50,5 +65,14 @@ public struct CoordinatorBuilder<Route: NavigationRoute>: View {
                 }
         }
         .environmentObject(coordinator)
+        .sheet(item: $coordinator.sheetRoute) { sheetRoute in
+            NavigationStack(path: $coordinator.path) {
+                sheetRoute.build()
+                    .navigationDestination(for: Route.self) { route in
+                        route.build()
+                    }
+            }
+            .environmentObject(coordinator)
+        }
     }
 }
